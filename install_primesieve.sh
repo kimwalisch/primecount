04d4b09e3b6d245678fff260f6d048a04f48f5ee
Kim Walisch <kim.walisch@gmail.com>
2013-06-16 23:07:24 +0300

    Updated intro text

--- install_primesieve.sh
+++ install_primesieve.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# This shell script builds and installs the latest primesieve
-# and libprimesieve version.
+# This shell script downloads, compiles and installs the
+# latest primesieve and libprimesieve version.
 # Copyright (C) 2013 Kim Walisch, <kim.walisch@gmail.com>
 # This file is distributed under the BSD License.
 # Usage: $ sh install_primesieve.sh [GNU make arguments]
