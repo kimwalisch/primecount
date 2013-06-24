5f6741a1cafdb212fa087b87281ed03ac7f382ef
Kim Walisch <kim.walisch@gmail.com>
2013-06-24 21:30:23 +0200

    Use primesieve's new (version 4.3) multi-threaded nthPrime() implementation for the last step

--- src/nth_prime.cpp
+++ src/nth_prime.cpp
@@ -1,38 +1,8 @@
 #include "isqrt.h"
 
 #include <primecount.h>
-#include <primesieve/soe/PrimeSieve.h>
-#include <primesieve/soe/PrimeSieveCallback.h>
-#include <primesieve/soe/stop_primesieve.h>
+#include <primesieve/soe/ParallelPrimeSieve.h>
 #include <stdint.h>
-
-namespace {
-
-/// This class is used to generate n primes and
-/// then stop by throwing an exception.
-///
-class FindNthPrime : public PrimeSieveCallback<uint64_t> {
-public:
-  FindNthPrime(uint64_t n) : n_(n), nthPrime_(0)
-  { }
-  void callback(uint64_t prime)
-  {
-    if (--n_ == 0)
-    {
-      nthPrime_ = prime;
-      throw stop_primesieve();
-    }
-  }
-  int64_t getNthPrime() const
-  {
-    return nthPrime_;
-  }
-private:
-  uint64_t n_;
-  uint64_t nthPrime_;
-};
-
-} // namespace
 
 namespace primecount {
 
@@ -46,22 +16,16 @@ int64_t nth_prime(int64_t n, int threads /* = MAX_THREADS */)
   if (n < 1)
     return 0;
 
-  int64_t nth_prime_approximation = Li_inverse(n);
-  int64_t a = nth_prime_approximation;
-  int64_t pia = pi(a, threads);
+  // this is < prime[n] up to ~ 10^316
+  int64_t nth_prime_approx = Li_inverse(n);
+  int64_t count_approx = pi(nth_prime_approx, threads);
 
-  FindNthPrime findNthPrime(n - pia);
-  try {
-    int64_t start = a;
-    int64_t stop = a + isqrt(a) * 2 + 10000;
-    PrimeSieve ps;
-    ps.generatePrimes(start, stop, &findNthPrime);
-  }
-  catch (stop_primesieve&) {
-    return findNthPrime.getNthPrime();
-  }
+  ParallelPrimeSieve pps;
+  if (threads != MAX_THREADS)
+    pps.setNumThreads( threads );
+  int64_t prime = pps.nthPrime(nth_prime_approx, n - count_approx);
 
-  return -1;
+  return prime;
 }
 
 } // namespace primecount
