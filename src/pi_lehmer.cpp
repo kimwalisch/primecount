9201aa8b576c4c03f860add9a260433f5ce8a8dc
Kim Walisch <kim.walisch@gmail.com>
2013-06-25 21:37:50 +0200

    Improved code readability

--- src/pi_lehmer.cpp
+++ src/pi_lehmer.cpp
@@ -12,9 +12,9 @@ int64_t pi_lehmer(int64_t x, int threads /* = MAX_THREADS */)
   if (x < 2)
     return 0;
 
-  int64_t c = pi_meissel(isqrt3(x));
-  int64_t a = pi_meissel(isqrt4(x));
-  int64_t b = pi_meissel(isqrt(x));
+  int64_t a = pi_legendre(isqrt4(x));
+  int64_t b = pi_legendre(isqrt(x));
+  int64_t c = pi_legendre(isqrt3(x));
 
   int64_t sum = 0;
 
