c27b395da793d164c05aed6c420d99fd9672157a
Kim Walisch <kim.walisch@gmail.com>
2013-07-08 11:44:21 +0200

    Added documentation

--- include/primecount.h
+++ include/primecount.h
@@ -12,9 +12,9 @@
 
 #include <stdint.h>
 
-#define PRIMECOUNT_VERSION "0.2"
+#define PRIMECOUNT_VERSION "0.3"
 #define PRIMECOUNT_MAJOR_VERSION 0
-#define PRIMECOUNT_MINOR_VERSION 2
+#define PRIMECOUNT_MINOR_VERSION 3
 #define PRIMECOUNT_YEAR "2013"
 
 namespace primecount {
@@ -24,16 +24,48 @@ enum {
   MAX_THREADS = -1
 };
 
+/// This is an alias for the fastest prime counting implementation
+/// within primecount.
+///
 int64_t pi(int64_t x, int threads = MAX_THREADS);
 
+/// Calculate the number of primes below x using an optimized 
+/// segmented sieve of Eratosthenes implementation.
+/// Run time: O(x log log x) operations, O(x^0.5) space.
+///
 int64_t pi_primesieve(int64_t x, int threads = MAX_THREADS);
+
+/// Calculate the number of primes below x using Legendre's formula.
+/// Run time: O(x) operations, O(x^0.5) space.
+///
 int64_t pi_legendre(int64_t x, int threads = MAX_THREADS);
+
+/// Calculate the number of primes below x using Meissel's formula.
+/// Run time: O(x/(log x)^3) operations, O(x^0.5/log x) space.
+///
 int64_t pi_meissel(int64_t x, int threads = MAX_THREADS);
+
+/// Calculate the number of primes below x using Lehmer's formula.
+/// Run time: O(x/(log x)^4) operations, O(x^0.5/log x) space.
+///
 int64_t pi_lehmer(int64_t x, int threads = MAX_THREADS);
 
+/// Calculate the nth prime using a combination of an efficient prime
+/// counting function implementation and the sieve of Eratosthenes.
+/// Run time: O(x/(log x)^4) operations, O(x^0.5) space.
+///
 int64_t nth_prime(int64_t n, int threads = MAX_THREADS);
 
+/// Calculate the offset logarithmic integral which is a very
+/// accurate approximation of the number of primes below x.
+/// @post Li(x) > pi(x) for 24 <= x <= ~ 10^316
+///
 int64_t Li(int64_t);
+
+/// Calculate the inverse logarithmic integral Li^-1(x) which is
+/// a very accurate approximation of the nth prime.
+/// @post Li_inverse(x) < nth_prime(x) for 7 <= x <= ~ 10^316
+///
 int64_t Li_inverse(int64_t);
 
 } // namespace primecount
