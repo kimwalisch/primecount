0a12349bb3a26f398972fdbde31d61f92bf1d13f
kimwalisch <kim.walisch@gmail.com>
2014-01-03 13:32:13 +0100

    Increase version to 0.11

--- include/primecount.hpp
+++ include/primecount.hpp
@@ -12,9 +12,9 @@
 
 #include <stdint.h>
 
-#define PRIMECOUNT_VERSION "0.10"
+#define PRIMECOUNT_VERSION "0.11"
 #define PRIMECOUNT_VERSION_MAJOR 0
-#define PRIMECOUNT_VERSION_MINOR 10
+#define PRIMECOUNT_VERSION_MINOR 11
 
 namespace primecount {
 
