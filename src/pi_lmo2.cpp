db1b7bdc03628b283a05de24a7cab512a20ac236
Kim Walisch <kim.walisch@gmail.com>
2014-03-31 13:40:08 +0200

    Update pi_lmo2.cpp

--- src/pi_lmo2.cpp
+++ src/pi_lmo2.cpp
@@ -48,22 +48,22 @@ int64_t S1(int64_t x,
 ///
 int64_t S2(int64_t x,
            int64_t x13_alpha,
-           int64_t x23_alpha,
            int64_t a,
            int64_t c,
            std::vector<int32_t>& primes,
            std::vector<int32_t>& lpf,
            std::vector<int32_t>& mu)
 {
+  int64_t limit = x / x13_alpha;
   int64_t S2_result = 0;
-  std::vector<char> sieve(x23_alpha + 1, 1);
+  std::vector<char> sieve(limit + 1, 1);
 
   // phi(y, b) nodes with b <= c do not contribute to S2, so we
   // simply sieve out the multiples of the first c primes
   for (int64_t b = 1; b <= c; b++)
   {
     int64_t prime = primes[b];
-    for (int64_t k = prime; k <= x23_alpha; k += prime)
+    for (int64_t k = prime; k <= limit; k += prime)
       sieve[k] = 0;
   }
 
@@ -90,7 +90,7 @@ int64_t S2(int64_t x,
     }
 
     // Remove the multiples of (b + 1)th prime
-    for (int64_t k = prime; k <= x23_alpha; k += prime * 2)
+    for (int64_t k = prime; k <= limit; k += prime * 2)
       sieve[k] = 0;
   }
 
@@ -103,8 +103,7 @@ namespace primecount {
 
 /// Calculate the number of primes below x using the
 /// Lagarias-Miller-Odlyzko algorithm.
-/// Run time: O(x^(2/3)) operations, O(x^(2/3)) space.
-/// @note O(x^(2/3)) space is because S2(x) is not segmented.
+/// Run time: O(x^(2/3)) operations, O(x^(2/3) / log log x) space.
 ///
 int64_t pi_lmo2(int64_t x, int threads)
 {
@@ -112,14 +111,13 @@ int64_t pi_lmo2(int64_t x, int threads)
     return 0;
 
   // Optimization factor, see:
-  // Tomás Oliveira e Silva, Computing pi(x): the combinatorial method,
-  // Revista do DETUA, vol. 4, no. 6, pp. 763-764, March 2006.
-  double beta = 1.1;
+  // J. C. Lagarias, V. S. Miller, and A. M. Odlyzko, Computing pi(x): The Meissel-
+  // Lehmer method, Mathematics of Computation, 44 (1985), p. 556.
+  double beta = 1.0;
   double alpha = std::max(1.0, log(log((double) x)) * beta);
 
   int64_t x13 = iroot<3>(x);
   int64_t x13_alpha = (int64_t)(x13 * alpha);
-  int64_t x23_alpha = (int64_t) std::pow((double) x / alpha, 2.0 / 3.0);
   int64_t a = pi_lehmer(x13_alpha);
   int64_t c = (a < 6) ? a : 6;
 
@@ -129,7 +127,7 @@ int64_t pi_lmo2(int64_t x, int threads)
   primes.push_back(0);
   primesieve::generate_n_primes(a, &primes);
 
-  int64_t phi = S1(x, x13_alpha, c, primes, lpf , mu) + S2(x, x13_alpha, x23_alpha, a, c, primes, lpf , mu);
+  int64_t phi = S1(x, x13_alpha, c, primes, lpf , mu) + S2(x, x13_alpha, a, c, primes, lpf , mu);
   int64_t sum = phi + a - 1 - P2(x, a, threads);
 
   return sum;
