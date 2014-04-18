6ea1c9c096cf7388ed7964ae5e62607e01d1ed96
Kim Walisch <kim.walisch@gmail.com>
2014-04-18 19:52:35 +0200

    Check if MSVC compiler supports OpenMP

---
+++ src/has_openmp.cpp
@@ -0,0 +1,19 @@
+///
+/// @file   has_openmp.cpp
+/// @brief  Used to check if MSVC compiler supports OpenMP.
+///
+/// Copyright (C) 2014 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#include <omp.h>
+#include <iostream>
+
+int main()
+{
+  std::cout << "OpenMP version (yyyymm) : " << _OPENMP               << std::endl
+            << "Number of CPU cores     : " << omp_get_max_threads() << std::endl;
+  return 0;
+}
