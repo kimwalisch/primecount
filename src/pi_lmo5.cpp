41bbf9c9f605acfd28a05566f5656ddee090b168
Kim Walisch <kim.walisch@gmail.com>
2014-05-03 12:28:33 +0200

    Update pi_lmo5.cpp

--- src/pi_lmo5.cpp
+++ src/pi_lmo5.cpp
@@ -17,20 +17,46 @@
 #include <primesieve.hpp>
 #include <stdint.h>
 #include <vector>
-#include <iostream>
 
-using std::log;
+using namespace std;
 
 namespace {
+
+/// Cross-off the multiples of prime in the sieve array.
+/// For each element that is unmarked the first time update
+/// the special counters tree data structure.
+///
+template <typename T1, typename T2>
+void cross_off(int64_t prime,
+               int64_t low,
+               int64_t high,
+               int64_t& next_multiple,
+               T1& sieve,
+               T2& counters)
+{
+  int64_t segment_size = sieve.size();
+  int64_t k = next_multiple;
+
+  for (; k < high; k += prime * 2)
+  {
+    if (sieve[k - low])
+    {
+      sieve[k - low] = 0;
+      cnt_update(counters, k - low, segment_size);
+    }
+  }
+
+  next_multiple = k;
+}
 
 /// Calculate the contribution of the ordinary leaves.
 ///
 int64_t S1(int64_t x,
            int64_t y,
            int64_t c,
-           std::vector<int32_t>& primes,
-           std::vector<int32_t>& lpf,
-           std::vector<int32_t>& mu)
+           vector<int32_t>& primes,
+           vector<int32_t>& lpf,
+           vector<int32_t>& mu)
 {
   int64_t S1_result = 0;
 
@@ -48,34 +74,34 @@ int64_t S2(int64_t x,
            int64_t y,
            int64_t pi_y,
            int64_t c,
-           std::vector<int32_t>& primes,
-           std::vector<int32_t>& lpf,
-           std::vector<int32_t>& mu)
+           vector<int32_t>& primes,
+           vector<int32_t>& lpf,
+           vector<int32_t>& mu)
 {
   int64_t limit = x / y + 1;
   int64_t segment_size = next_power_of_2(isqrt(limit));
   int64_t sqrt_y = isqrt(y);
   int64_t S2_result = 0;
 
-  // vector used for sieving
-  std::vector<char> sieve(segment_size);
-  std::vector<int32_t> counters(segment_size);
-  std::vector<int32_t> pi = make_pi(y);
-  std::vector<int32_t> l_max(primes.size(), primes.size() - 1);
-  std::vector<int64_t> next(primes.begin(), primes.end());
-  std::vector<int64_t> phi(primes.size(), 0);
+  vector<char> sieve(segment_size);
+  vector<int32_t> counters(segment_size);
+  vector<int32_t> pi = make_pi(y);
+  vector<int32_t> l_max(primes.size(), pi_y);
+  vector<int64_t> next(primes.begin(), primes.end());
+  vector<int64_t> phi(primes.size(), 0);
 
   // Segmented sieve of Eratosthenes
   for (int64_t low = 1; low < limit; low += segment_size)
   {
-    std::fill(sieve.begin(), sieve.end(), 1);
+    fill(sieve.begin(), sieve.end(), 1);
 
     // Current segment = interval [low, high[
-    int64_t high = std::min(low + segment_size, limit);
+    int64_t high = min(low + segment_size, limit);
+    int64_t b = 1;
 
     // phi(y, b) nodes with b <= c do not contribute to S2, so we
     // simply sieve out the multiples of the first c primes
-    for (int64_t b = 1; b <= c; b++)
+    for (; b <= c; b++)
     {
       int64_t k = next[b];
       for (int64_t prime = primes[b]; k < high; k += prime)
@@ -86,73 +112,58 @@ int64_t S2(int64_t x,
     // Initialize special tree data structure from sieve
     cnt_finit(sieve, counters, segment_size);
 
-    int64_t b = c;
-    for (; b + 1 < pi[sqrt_y]; b++)
+    for (; b < pi[sqrt_y]; b++)
     {
-      int64_t prime = primes[b + 1];
-      int64_t min_m = std::max(x / (high * prime), y / prime);
-      int64_t max_m = std::min(x / (low * prime), y);
+      int64_t prime = primes[b];
+      int64_t m_min = max(x / (prime * high), y / prime);
+      int64_t m_max = min(x / (prime * low), y);
 
-      if (prime >= max_m)
+      if (prime >= m_max)
         break;
 
-      for (int64_t m = max_m; m > min_m; m--)
+      for (int64_t m = m_max; m > m_min; m--)
       {
         if (mu[m] != 0 && prime < lpf[m])
         {
           int64_t n = prime * m;
           int64_t count = cnt_query(counters, (x / n) - low);
-          int64_t phi_xn = phi[b + 1] + count;
-
+          int64_t phi_xn = phi[b] + count;
           S2_result -= mu[m] * phi_xn;
         }
       }
 
-      phi[b + 1] += cnt_query(counters, (high - 1) - low);
+      // Needed for calculation of special leaves in next segment
+      phi[b] += cnt_query(counters, (high - 1) - low);
 
-      int64_t k = next[b + 1];
-      for (; k < high; k += prime * 2)
-      {
-        if (sieve[k - low])
-        {
-          sieve[k - low] = 0;
-          cnt_update(counters, k - low, segment_size);
-        }
-      }
-      next[b + 1] = k;
+      // Cross-off multiples of prime
+      cross_off(prime, low, high, next[b], sieve, counters);
     }
 
-    int64_t special_leaf_threshold = std::max(x / high, y);
+    int64_t special_leaf_threshold = max(x / high, y);
 
-    for (; b + 1 < pi_y; b++)
+    for (; b < pi_y; b++)
     {
-      int64_t prime = primes[b + 1];
-      int64_t l = l_max[b + 1];
-      special_leaf_threshold = std::max(prime * prime, special_leaf_threshold);
+      int64_t prime = primes[b];
+      int64_t l = l_max[b];
 
       if (prime >= primes[l])
         break;
 
+      special_leaf_threshold = max(prime * prime, special_leaf_threshold);
+
       for (; prime * primes[l] > special_leaf_threshold; l--)
       {
         int64_t n = prime * primes[l];
-        int64_t phi_xn = phi[b + 1] + cnt_query(counters, (x / n) - low);
+        int64_t count = cnt_query(counters, (x / n) - low);
+        int64_t phi_xn = phi[b] + count;
         S2_result += phi_xn;
       }
 
-      l_max[b + 1] = l;
-      phi[b + 1] += cnt_query(counters, (high - 1) - low);
+      // Save for use in next segment
+      l_max[b] = l;
+      phi[b] += cnt_query(counters, (high - 1) - low);
 
-      int64_t k = next[b + 1];
-      for (; k < high; k += prime * 2)
-      {
-        if (sieve[k - low])
-        {
-          sieve[k - low] = 0;
-          cnt_update(counters, k - low, segment_size);
-        }
-      }
-      next[b + 1] = k;
+      cross_off(prime, low, high, next[b], sieve, counters);
     }
   }
 
@@ -176,17 +187,17 @@ int64_t pi_lmo5(int64_t x, int threads)
   // Optimization factor, see:
   // J. C. Lagarias, V. S. Miller, and A. M. Odlyzko, Computing pi(x): The Meissel-
   // Lehmer method, Mathematics of Computation, 44 (1985), p. 556.
-  double beta = 0.6;
-  double alpha = std::max(1.0, log(log((double) x)) * beta);
+  double beta = 1.0;
+  double alpha = max(1.0, log(log((double) x)) * beta);
 
   int64_t x13 = iroot<3>(x);
   int64_t y = (int64_t)(x13 * alpha);
   int64_t a = pi_lehmer(y);
   int64_t c = (a < 6) ? a : 6;
 
-  std::vector<int32_t> lpf = make_least_prime_factor(y);
-  std::vector<int32_t> mu = make_moebius(y);
-  std::vector<int32_t> primes;
+  vector<int32_t> lpf = make_least_prime_factor(y);
+  vector<int32_t> mu = make_moebius(y);
+  vector<int32_t> primes;
   primes.push_back(0);
   primesieve::generate_n_primes(a, &primes);
 
