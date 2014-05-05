6b0f407cfa5cc9322b0208e52f992a07b8c7e0cb
Kim Walisch <kim.walisch@gmail.com>
2014-05-05 10:56:55 +0200

    Update pi_lmo5.cpp

--- src/pi_lmo5.cpp
+++ src/pi_lmo5.cpp
@@ -12,6 +12,7 @@
 #include "Pk.hpp"
 #include "pmath.hpp"
 #include "tos_counters.hpp"
+#include "pi_bsearch.hpp"
 
 #include <primecount.hpp>
 #include <primesieve.hpp>
@@ -35,18 +36,18 @@ void cross_off(int64_t prime,
                T2& counters)
 {
   int64_t segment_size = sieve.size();
-  int64_t i;
+  int64_t k = next_multiple;
 
-  for (i = next_multiple - low; i < segment_size; i += prime * 2)
+  for (; k < high; k += prime * 2)
   {
-    if (sieve[i])
+    if (sieve[k - low])
     {
-      sieve[i] = 0;
-      cnt_update(counters, i, segment_size);
+      sieve[k - low] = 0;
+      cnt_update(counters, k - low, segment_size);
     }
   }
 
-  next_multiple = low + i;
+  next_multiple = k;
 }
 
 /// Calculate the contribution of the ordinary leaves.
@@ -80,12 +81,11 @@ int64_t S2(int64_t x,
 {
   int64_t limit = x / y + 1;
   int64_t segment_size = next_power_of_2(isqrt(limit));
-  int64_t sqrt_y = isqrt(y);
+  int64_t pi_sqrty = pi_bsearch(primes, isqrt(y));
   int64_t S2_result = 0;
 
   vector<char> sieve(segment_size);
   vector<int32_t> counters(segment_size);
-  vector<int32_t> pi = make_pi(y);
   vector<int32_t> l_max(primes.size(), pi_y);
   vector<int64_t> next(primes.begin(), primes.end());
   vector<int64_t> phi(primes.size(), 0);
@@ -97,6 +97,7 @@ int64_t S2(int64_t x,
 
     // Current segment = interval [low, high[
     int64_t high = min(low + segment_size, limit);
+    int64_t special_leaf_threshold = max(x / high, y);
     int64_t b = 1;
 
     // phi(y, b) nodes with b <= c do not contribute to S2, so we
@@ -112,16 +113,16 @@ int64_t S2(int64_t x,
     // Initialize special tree data structure from sieve
     cnt_finit(sieve, counters, segment_size);
 
-    for (; b < pi[sqrt_y]; b++)
+    for (; b < pi_sqrty; b++)
     {
       int64_t prime = primes[b];
-      int64_t m_min = max(x / (prime * high), y / prime);
-      int64_t m_max = min(x / (prime * low), y);
-
-      if (prime >= m_max)
+      int64_t m = std::min(x / (prime * low), y);
+      if (prime >= m)
         break;
 
-      for (int64_t m = m_max; m > m_min; m--)
+      special_leaf_threshold = max(prime * prime, special_leaf_threshold);
+
+      for (; prime * m > special_leaf_threshold; m--)
       {
         if (mu[m] != 0 && prime < lpf[m])
         {
@@ -138,8 +139,6 @@ int64_t S2(int64_t x,
       // Cross-off multiples of prime
       cross_off(prime, low, high, next[b], sieve, counters);
     }
-
-    int64_t special_leaf_threshold = max(x / high, y);
 
     for (; b < pi_y; b++)
     {
