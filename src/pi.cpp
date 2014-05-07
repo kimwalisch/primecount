c5614c799c82b57266cdf19e6f024fb1fdd52721
Kim Walisch <kim.walisch@gmail.com>
2014-05-07 15:17:48 +0200

    Update pi.cpp

--- src/pi.cpp
+++ src/pi.cpp
@@ -15,7 +15,7 @@ namespace primecount {
 /// Alias for fastest pi(x) implementation
 int64_t pi(int64_t x, int threads /* = MAX_THREADS */)
 {
-  return pi_lmo5(x, threads);
+  return pi_lmo(x, threads);
 }
 
 } // namespace primecount
