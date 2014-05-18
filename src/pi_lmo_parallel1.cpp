00d1afed08627fb1bf3d87d4385b7dcbae5c63e0
Kim Walisch <kim.walisch@gmail.com>
2014-05-18 10:30:02 -0700

    Update pi_lmo_parallel1.cpp

--- src/pi_lmo_parallel1.cpp
+++ src/pi_lmo_parallel1.cpp
@@ -213,7 +213,7 @@ int64_t pi_lmo_parallel1(int64_t x, int threads)
   if (x < 2)
     return 0;
 
-  double beta = 0.5;
+  double beta = 0.6;
   double alpha = max(1.0, log(log((double) x)) * beta);
   int64_t x13 = iroot<3>(x);
   int64_t y = (int64_t)(x13 * alpha);
