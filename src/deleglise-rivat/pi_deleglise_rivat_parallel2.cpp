031e6eb55736b4cdfdd6f83ae55d42d7ab790d97
Kim Walisch <kim.walisch@gmail.com>
2014-06-08 10:31:29 +0200

    Refactoring

--- src/deleglise-rivat/pi_deleglise_rivat_parallel2.cpp
+++ src/deleglise-rivat/pi_deleglise_rivat_parallel2.cpp
@@ -167,10 +167,10 @@ int64_t S2_thread(int64_t x,
 
       int64_t min_m = max(x / (prime * high), y / prime);
       min_m = in_between(prime, min_m, y);
-      int64_t min_hard_leaf = pi[min_m];
       int64_t min_trivial_leaf = pi[min(x / (prime * prime), y)];
       int64_t min_clustered_easy_leaf = pi[min(isqrt(x / prime), y)];
       int64_t min_sparse_easy_leaf = pi[min(z / prime, y)];
+      int64_t min_hard_leaf = pi[min_m];
 
       min_trivial_leaf = max(min_hard_leaf, min_trivial_leaf);
       min_clustered_easy_leaf = max(min_hard_leaf, min_clustered_easy_leaf);
