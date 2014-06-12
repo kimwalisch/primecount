b34cc80c4fd2cddda62c48454c633a71f994c4bf
Kim Walisch <kim.walisch@gmail.com>
2014-06-12 10:18:16 +0200

    Create balance_S2_load.hpp

---
+++ include/balance_S2_load.hpp
@@ -0,0 +1,26 @@
+///
+/// @file   balance_S2_load.hpp
+///
+/// Copyright (C) 2014 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#ifndef BALANCE_S2_LOAD_HPP
+#define BALANCE_S2_LOAD_HPP
+
+#include <aligned_vector.hpp>
+#include <stdint.h>
+
+namespace primecount {
+
+void balance_S2_load(int64_t* segment_size,
+                     int64_t* segments_per_thread,
+                     int64_t min_segment_size,
+                     int64_t max_segment_size,
+                     aligned_vector<double>& timings);
+
+} // namespace primecount
+
+#endif
