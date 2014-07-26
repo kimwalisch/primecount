eb5826f1d62310109b0c2e35b5679563b4566369
Kim Walisch <kim.walisch@gmail.com>
2014-07-26 15:55:05 +0200

    More 128-bit support

---
+++ include/64-bit/primecount-internal.hpp
@@ -0,0 +1,232 @@
+///
+/// @file   primecount-internal.hpp
+/// @brief  primecount internal function definitions.
+///
+/// Copyright (C) 2014 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#ifndef PRIMECOUNT_INTERNAL_HPP
+#define PRIMECOUNT_INTERNAL_HPP
+
+#include <stdint.h>
+#include <vector>
+
+namespace primecount {
+
+enum {
+  /// Uses all CPU cores.
+  MAX_THREADS = -1
+};
+
+class PhiCache;
+class FactorTable;
+
+/// This is an alias for the fastest prime counting implementation
+/// within primecount.
+///
+int64_t pi(int64_t x, int threads);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat(int64_t x, int threads);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat1(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat2(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat3(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat_parallel1(int64_t x, int threads);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat_parallel2(int64_t x, int threads);
+
+/// Calculate the number of primes below x using the
+/// Deleglise-Rivat algorithm.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/3) * (log x)^3) space.
+///
+int64_t pi_deleglise_rivat_parallel3(int64_t x, int threads);
+
+/// Calculate the number of primes below x using Legendre's formula.
+/// Run time: O(x) operations, O(x^(1/2)) space.
+///
+int64_t pi_legendre(int64_t x, int threads);
+
+/// Calculate the number of primes below x using Lehmer's formula.
+/// Run time: O(x/(log x)^4) operations, O(x^(1/2)) space.
+///
+int64_t pi_lehmer(int64_t x, int threads);
+
+/// Calculate the number of primes below x using Lehmer's formula.
+/// This version uses a different P2(x, y) implementation,
+/// it runs slower than pi_lehmer(x) on most systems.
+/// Run time: O(x/(log x)^4) operations, O(x^(1/2) / log x) space.
+///
+int64_t pi_lehmer2(int64_t x, int threads);
+
+/// Parallel implementation of the Lagarias-Miller-Odlyzko
+/// prime counting algorithm using OpenMP.
+/// Run time: O(x^(2/3) / log x) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo(int64_t x, int threads);
+
+/// Calculate the number of primes below x using the
+/// Lagarias-Miller-Odlyzko algorithm. This implementation uses the
+/// recursive phi formula with caching to calculate S2(x).
+/// Run time: O(x^(2/3)) operations, O(x^(1/3)) space.
+///
+int64_t pi_lmo1(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Lagarias-Miller-Odlyzko algorithm. This implementation uses
+/// the sieve of Eratosthenes to calculate S2(x).
+/// Run time: O(x^(2/3)) operations, O(x^(2/3) / (log x)^2) space.
+///
+int64_t pi_lmo2(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Lagarias-Miller-Odlyzko algorithm. This implementation uses
+/// the segmented sieve of Eratosthenes to calculate S2(x).
+/// Run time: O(x^(2/3)) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo3(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Lagarias-Miller-Odlyzko algorithm. This implementation uses
+/// the segmented sieve of Eratosthenes and a special data structure
+/// for counting to calculate S2(x).
+/// Run time: O(x^(2/3)) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo4(int64_t x);
+
+/// Calculate the number of primes below x using the
+/// Lagarias-Miller-Odlyzko algorithm.
+/// Run time: O(x^(2/3) / log x) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo5(int64_t x);
+
+/// Parallel implementation of the Lagarias-Miller-Odlyzko
+/// prime counting algorithm using OpenMP.
+/// Run time: O(x^(2/3)) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo_parallel1(int64_t x, int threads);
+
+/// Parallel implementation of the Lagarias-Miller-Odlyzko
+/// prime counting algorithm using OpenMP.
+/// Run time: O(x^(2/3) / log x) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo_parallel2(int64_t x, int threads);
+
+/// Parallel implementation of the Lagarias-Miller-Odlyzko
+/// prime counting algorithm using OpenMP.
+/// Run time: O(x^(2/3) / log x) operations, O(x^(1/3) * (log x)^2) space.
+///
+int64_t pi_lmo_parallel3(int64_t x, int threads);
+
+/// Calculate the number of primes below x using Meissel's formula.
+/// Run time: O(x/(log x)^3) operations, O(x^(1/2) / log x) space.
+///
+int64_t pi_meissel(int64_t x, int threads);
+
+/// Calculate the number of primes below x using an optimized 
+/// segmented sieve of Eratosthenes implementation.
+/// Run time: O(x log log x) operations, O(x^(1/2)) space.
+///
+int64_t pi_primesieve(int64_t x, int threads);
+
+/// Partial sieve function (a.k.a. Legendre-sum).
+/// phi(x, a) counts the numbers <= x that are not divisible
+/// by any of the first a primes.
+///
+int64_t phi(int64_t x, int64_t a, int threads);
+
+/// Partial sieve function (a.k.a. Legendre-sum).
+/// phi(x, a) counts the numbers <= x that are not divisible
+/// by any of the first a primes.
+///
+int64_t phi(int64_t x, int64_t a, PhiCache* phiCache);
+
+/// Calculate the offset logarithmic integral which is a very
+/// accurate approximation of the number of primes below x.
+/// @post Li(x) > pi(x) for 24 <= x <= ~ 10^316
+///
+int64_t Li(int64_t);
+
+/// Calculate the inverse logarithmic integral Li^-1(x) which is
+/// a very accurate approximation of the nth prime.
+/// @post Li_inverse(x) < nth_prime(x) for 7 <= x <= ~ 10^316
+///
+int64_t Li_inverse(int64_t);
+
+/// Calculate the nth prime using a combination of an efficient prime
+/// counting function implementation and the sieve of Eratosthenes.
+/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/2)) space.
+///
+int64_t nth_prime(int64_t n, int threads);
+
+/// P2(x, a) counts the numbers <= x that have exactly 2 prime
+/// factors each exceeding the a-th prime.
+/// Space complexity: O((x / primes[a])^(1/2)).
+///
+int64_t P2(int64_t x, int64_t y, int threads);
+
+/// P2_lehmer(x, a) counts the numbers <= x that have exactly 2
+/// prime factors each exceeding the a-th prime.
+/// Space complexity: O(pi(x^(1/2))).
+///
+int64_t P2_lehmer(int64_t x, int64_t a, int threads);
+
+/// P3(x, a) counts the numbers <= x that have exactly 3
+/// prime factorseach exceeding the a-th prime.
+/// Space complexity: O(pi(x^(1/2))).
+///
+int64_t P3(int64_t x, int64_t a, int threads);
+
+/// Calculate the contribution of the ordinary leaves in the
+/// Lagarias-Miller-Odlyzko algorithm.
+/// Run time: O(y) operations, O(y) space.
+///
+int64_t S1(int64_t x,
+           int64_t y,
+           int64_t c,
+           std::vector<int32_t>& primes,
+           std::vector<int32_t>& lpf,
+           std::vector<int32_t>& mu);
+
+/// Calculate the contribution of the ordinary leaves in the
+/// Lagarias-Miller-Odlyzko algorithm.
+/// Run time: O(y) operations, O(y) space.
+///
+int64_t S1(int64_t x,
+           int64_t y,
+           int64_t c,
+           std::vector<int32_t>& primes,
+           FactorTable& Factors);
+
+} // namespace primecount
+
+#endif
---
+++ src/64-bit/test.cpp
@@ -0,0 +1,147 @@
+///
+/// @file  test.cpp
+///
+/// Copyright (C) 2014 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#include <primecount.hpp>
+#include <primecount-internal.hpp>
+#include <primesieve.hpp>
+
+#include <stdint.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stdexcept>
+#include <sstream>
+#include <ctime>
+
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
+
+/// For types: f1(x) , f2(x)
+#define CHECK_11(f1, f2) check_equal(#f1, x, f1 (x), f2 (x))
+
+/// For types: f1(x) , f2(x, threads)
+#define CHECK_12(f1, f2) check_equal(#f1, x, f1 (x), f2 (x, get_num_threads()))
+
+/// For types: f1(x, threads) , f2(x, threads)
+#define CHECK_22(f1, f2) check_equal(#f1, x, f1 (x, get_num_threads()), f2 (x, get_num_threads()))
+
+#define CHECK_EQUAL(f1, f2, check, iters) \
+{ \
+  cout << "Testing " << #f1 << "(x)" << flush; \
+ \
+  /* test for 0 <= x < 10000 */ \
+  for (int64_t x = 0; x < 10000; x++) \
+    check(f1, f2); \
+ \
+  int64_t x = 0; \
+  /* test using random increment */ \
+  for (int64_t i = 0; i < iters; i++, x += get_rand()) \
+  { \
+    check(f1, f2); \
+    double percent = 100.0 * (i + 1.0) / iters; \
+    cout << "\rTesting " << #f1 "(x) " << (int) percent << "%" << flush; \
+  } \
+ \
+  cout << endl; \
+}
+
+using namespace std;
+using namespace primecount;
+using primesieve::parallel_nth_prime;
+
+namespace {
+
+int get_rand()
+{
+  // 0 <= get_rand() < 10^7
+  return (rand() % 10000) * 1000 + 1;
+}
+
+void check_equal(const string& f1, int64_t x, int64_t res1, int64_t res2)
+{
+  if (res1 != res2)
+  {
+    ostringstream oss;
+    oss << f1 << "(" << x << ") = " << res1
+        << " is an error, the correct result is " << res2;
+    throw runtime_error(oss.str());
+  }
+}
+
+void test_phi_thread_safety(int64_t iters)
+{
+#ifdef _OPENMP
+  cout << "Testing phi(x, a)" << flush;
+
+  int nested_threads = 2;
+  int64_t single_thread_sum = 0;
+  int64_t multi_thread_sum = 0;
+  int64_t base = 1000000;
+
+  omp_set_nested(true);
+
+  #pragma omp parallel for reduction(+: multi_thread_sum)
+  for (int64_t i = 0; i < iters; i++)
+    multi_thread_sum += pi_legendre(base + i, nested_threads);
+
+  omp_set_nested(false);
+
+  for (int64_t i = 0; i < iters; i++)
+    single_thread_sum += pi_legendre(base + i, 1);
+
+  if (multi_thread_sum != single_thread_sum)
+    throw runtime_error("Error: multi-threaded phi(x, a) is broken.");
+
+  std::cout << "\rTesting phi(x, a) 100%" << endl;
+#endif
+}
+
+} // namespace
+
+namespace primecount {
+
+bool test()
+{
+  srand((unsigned) time(0));
+  try
+  {
+    test_phi_thread_safety(100);
+
+    CHECK_EQUAL(pi_legendre,                  pi_primesieve,      CHECK_22, 100);
+    CHECK_EQUAL(pi_meissel,                   pi_legendre,        CHECK_22, 400);
+    CHECK_EQUAL(pi_lehmer,                    pi_meissel,         CHECK_22, 400);
+    CHECK_EQUAL(pi_lehmer2,                   pi_lehmer,          CHECK_22, 200);
+    CHECK_EQUAL(pi_lmo1,                      pi_meissel,         CHECK_12, 200);
+    CHECK_EQUAL(pi_lmo2,                      pi_meissel,         CHECK_12, 200);
+    CHECK_EQUAL(pi_lmo3,                      pi_meissel,         CHECK_12, 300);
+    CHECK_EQUAL(pi_lmo4,                      pi_meissel,         CHECK_12, 300);
+    CHECK_EQUAL(pi_lmo5,                      pi_meissel,         CHECK_12, 400);
+    CHECK_EQUAL(pi_lmo_parallel1,             pi_meissel,         CHECK_22, 400);
+    CHECK_EQUAL(pi_lmo_parallel2,             pi_meissel,         CHECK_22, 400);
+    CHECK_EQUAL(pi_lmo_parallel3,             pi_meissel,         CHECK_22, 400);
+    CHECK_EQUAL(pi_deleglise_rivat1,          pi_lmo_parallel3,   CHECK_12, 600);
+    CHECK_EQUAL(pi_deleglise_rivat2,          pi_lmo_parallel3,   CHECK_12, 600);
+    CHECK_EQUAL(pi_deleglise_rivat3,          pi_lmo_parallel3,   CHECK_12, 600);
+    CHECK_EQUAL(pi_deleglise_rivat_parallel1, pi_lmo_parallel3,   CHECK_22, 900);
+    CHECK_EQUAL(pi_deleglise_rivat_parallel2, pi_lmo_parallel3,   CHECK_22, 900);
+    CHECK_EQUAL(pi_deleglise_rivat_parallel3, pi_lmo_parallel3,   CHECK_22, 900);
+    CHECK_EQUAL(nth_prime,                    parallel_nth_prime, CHECK_11,  70);
+  }
+  catch (runtime_error& e)
+  {
+    cerr << endl << e.what() << endl;
+    return false;
+  }
+
+  cout << "All tests passed successfully!" << endl;
+  return true;
+}
+
+} // namespace primecount
