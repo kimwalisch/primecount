14147669770ec75938762f5c78307c7e2a4e8109
Kim Walisch <kim.walisch@gmail.com>
2014-08-03 20:47:34 +0200

    Refactoring

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -37,71 +37,75 @@ int64_t int64_cast(maxint_t x)
 int main (int argc, char* argv[])
 {
   PrimeCountOptions pco = parseOptions(argc, argv);
-  maxint_t res = 0;
   double time = get_wtime();
+
+  maxint_t x = pco.x;
+  maxint_t a = pco.a;
+  maxint_t res = 0;
+  int threads = pco.threads;
 
   try
   {
     switch (pco.option)
     {
       case OPTION_DELEGLISE_RIVAT:
-        res = pi_deleglise_rivat(pco.x, pco.threads); break;
+        res = pi_deleglise_rivat(x, threads); break;
       case OPTION_DELEGLISE_RIVAT1:
-        res = pi_deleglise_rivat1(int64_cast(pco.x)); break;
+        res = pi_deleglise_rivat1(int64_cast(x)); break;
       case OPTION_DELEGLISE_RIVAT2:
-        res = pi_deleglise_rivat2(int64_cast(pco.x)); break;
+        res = pi_deleglise_rivat2(int64_cast(x)); break;
       case OPTION_DELEGLISE_RIVAT3:
-        res = pi_deleglise_rivat3(int64_cast(pco.x)); break;
+        res = pi_deleglise_rivat3(int64_cast(x)); break;
       case OPTION_DELEGLISE_RIVAT_PARALLEL1:
-        res = pi_deleglise_rivat_parallel1(int64_cast(pco.x), pco.threads); break;
+        res = pi_deleglise_rivat_parallel1(int64_cast(x), threads); break;
       case OPTION_DELEGLISE_RIVAT_PARALLEL2:
-        res = pi_deleglise_rivat_parallel2(int64_cast(pco.x), pco.threads); break;
+        res = pi_deleglise_rivat_parallel2(int64_cast(x), threads); break;
       case OPTION_DELEGLISE_RIVAT_PARALLEL3:
-        res = pi_deleglise_rivat_parallel3(int64_cast(pco.x), pco.threads); break;
+        res = pi_deleglise_rivat_parallel3(int64_cast(x), threads); break;
       case OPTION_LEGENDRE:
-        res = pi_legendre(int64_cast(pco.x), pco.threads); break;
+        res = pi_legendre(int64_cast(x), threads); break;
       case OPTION_LEHMER:
-        res = pi_lehmer(int64_cast(pco.x), pco.threads); break;
+        res = pi_lehmer(int64_cast(x), threads); break;
       case OPTION_LEHMER2:
-        res = pi_lehmer2(int64_cast(pco.x), pco.threads); break;
+        res = pi_lehmer2(int64_cast(x), threads); break;
       case OPTION_LMO:
-        res = pi_lmo(int64_cast(pco.x), pco.threads); break;
+        res = pi_lmo(int64_cast(x), threads); break;
       case OPTION_LMO1:
-        res = pi_lmo1(int64_cast(pco.x)); break;
+        res = pi_lmo1(int64_cast(x)); break;
       case OPTION_LMO2:
-        res = pi_lmo2(int64_cast(pco.x)); break;
+        res = pi_lmo2(int64_cast(x)); break;
       case OPTION_LMO3:
-        res = pi_lmo3(int64_cast(pco.x)); break;
+        res = pi_lmo3(int64_cast(x)); break;
       case OPTION_LMO4:
-        res = pi_lmo4(int64_cast(pco.x)); break;
+        res = pi_lmo4(int64_cast(x)); break;
       case OPTION_LMO5:
-        res = pi_lmo5(int64_cast(pco.x)); break;
+        res = pi_lmo5(int64_cast(x)); break;
       case OPTION_LMO_PARALLEL1:
-        res = pi_lmo_parallel1(int64_cast(pco.x), pco.threads); break;
+        res = pi_lmo_parallel1(int64_cast(x), threads); break;
       case OPTION_LMO_PARALLEL2:
-        res = pi_lmo_parallel2(int64_cast(pco.x), pco.threads); break;
+        res = pi_lmo_parallel2(int64_cast(x), threads); break;
       case OPTION_LMO_PARALLEL3:
-        res = pi_lmo_parallel3(int64_cast(pco.x), pco.threads); break;
+        res = pi_lmo_parallel3(int64_cast(x), threads); break;
       case OPTION_MEISSEL:
-        res = pi_meissel(int64_cast(pco.x), pco.threads); break;
+        res = pi_meissel(int64_cast(x), threads); break;
       case OPTION_PRIMESIEVE:
-        res = pi_primesieve(int64_cast(pco.x), pco.threads); break;
+        res = pi_primesieve(int64_cast(x), threads); break;
       case OPTION_PHI:
-        res = phi(int64_cast(pco.x), int64_cast(pco.a)); break;
+        res = phi(int64_cast(x), int64_cast(a)); break;
       case OPTION_PI:
-        res = pi(pco.x, pco.threads); break;
+        res = pi(x, threads); break;
       case OPTION_LI:
-        res = Li(int64_cast(pco.x)); break;
+        res = Li(int64_cast(x)); break;
       case OPTION_LIINV:
-        res = Li_inverse(int64_cast(pco.x)); break;
+        res = Li_inverse(int64_cast(x)); break;
       case OPTION_NTHPRIME:
-        res = nth_prime(int64_cast(pco.x), pco.threads); break;
+        res = nth_prime(int64_cast(x), threads); break;
 
 #ifdef HAVE_INT128_T
       case OPTION_DELEGLISE_RIVAT4:
-        res = pi_deleglise_rivat4(pco.x); break;
+        res = pi_deleglise_rivat4(x); break;
       case OPTION_DELEGLISE_RIVAT_PARALLEL4:
-        res = pi_deleglise_rivat_parallel4(pco.x, pco.threads); break;
+        res = pi_deleglise_rivat_parallel4(x, threads); break;
 #endif
     }
   }
