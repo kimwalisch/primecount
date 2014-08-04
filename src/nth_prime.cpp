eee0e9dfc342f228967fccf96b9b02cbcac12e03
Kim Walisch <kim.walisch@gmail.com>
2014-08-04 10:32:18 +0200

    Fix off by 1 error

--- src/nth_prime.cpp
+++ src/nth_prime.cpp
@@ -1,5 +1,6 @@
 ///
 /// @file  nth_prime.cpp
+/// @brief Find the nth prime
 ///
 /// Copyright (C) 2014 Kim Walisch, <kim.walisch@gmail.com>
 ///
@@ -13,31 +14,39 @@
 #include <pmath.hpp>
 
 #include <stdint.h>
+#include <iostream>
 
 namespace primecount {
 
-/// Calculate the nth prime using a combination of an efficient prime
-/// counting function implementation and the sieve of Eratosthenes.
-/// Run time: O(x^(2/3) / (log x)^2) operations, O(x^(1/2)) space.
+/// Find the nth prime using a combination of the Deleglise-Rivat
+/// prime counting algorithm and the segmented sieve of Eratosthenes.
+/// Run time: O(x^(2/3) / (log x)^2), space: O(x^(1/2)).
 ///
 int64_t nth_prime(int64_t n, int threads)
 {
   if (n < 1)
     n = 1;
 
+  int64_t prime = 0;
   int64_t prime_approx = 0;
   int64_t count_approx = 0;
 
-  if (n > 100000)
+  primesieve::set_num_threads(threads);
+
+  if (n < 100000)
+    prime = primesieve::parallel_nth_prime(n, 0);
+  else
   {
     // Formula due to Dana Jacobsen:
     // Nth prime ~ Li^-1(n) + Li^-1(sqrt(n)) / 4
     prime_approx = Li_inverse(n) + Li_inverse(isqrt(n)) / 4;
     count_approx = pi(prime_approx, threads);
-  }
 
-  primesieve::set_num_threads(threads);
-  int64_t prime = primesieve::parallel_nth_prime(n - count_approx, prime_approx);
+    if (count_approx <= n)
+      prime = primesieve::parallel_nth_prime(n - count_approx, prime_approx);
+    else /* count_approx > n */
+      prime = primesieve::parallel_nth_prime(n - count_approx - 1, prime_approx + 1);
+  }
 
   return prime;
 }
