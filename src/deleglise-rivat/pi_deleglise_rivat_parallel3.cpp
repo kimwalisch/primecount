f6a5fb6f5389eca993cfe6b052e1a0931e5e7369
Kim Walisch <kim.walisch@gmail.com>
2014-12-08 09:48:40 +0100

    Moved S2_trivial(x, y) to S2_trivial.cpp

--- src/deleglise-rivat/pi_deleglise_rivat_parallel3.cpp
+++ src/deleglise-rivat/pi_deleglise_rivat_parallel3.cpp
@@ -31,53 +31,10 @@
 #include <iostream>
 #include <vector>
 
-#ifdef _OPENMP
-  #include <omp.h>
-#endif
-
 using namespace std;
 using namespace primecount;
 
 namespace {
-
-/// Calculate the contribution of the trivial leaves.
-///
-template <typename P>
-int128_t S2_trivial(uint128_t x,
-                   int64_t y,
-                   int64_t z,
-                   int64_t c,
-                   PiTable& pi,
-                   vector<P>& primes,
-                   int threads)
-{
-  if (print_status())
-  {
-    cout << endl;
-    cout << "=== S2_trivial(x, y) ===" << endl;
-    cout << "Computation of the trivial special leaves" << endl;
-  }
-
-  int64_t pi_y = pi[y];
-  int64_t pi_sqrtz = pi[min(isqrt(z), y)];
-  int128_t S2_total = 0;
-  double time = get_wtime();
-
-  // Find all trivial leaves: n = primes[b] * primes[l]
-  // which satisfy phi(x / n), b - 1) = 1
-  #pragma omp parallel for num_threads(threads) reduction(+: S2_total)
-  for (int64_t b = max(c, pi_sqrtz + 1); b < pi_y; b++)
-  {
-    uint128_t prime = primes[b];
-    uint64_t xn = (uint64_t) max(x / (prime * prime), prime);
-    S2_total += pi_y - pi[xn];
-  }
-
-  if (print_status())
-    print_result("S2_trivial", S2_total, time);
-
-  return S2_total;
-}
 
 /// Calculate the contribution of the special leaves.
 /// @pre y > 0 && c > 1
