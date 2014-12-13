a9e610dc9d2b6b21e95f8f68babc3eb640fbff87
Kim Walisch <kim.walisch@gmail.com>
2014-12-13 12:02:02 +0100

    Update

--- src/P2.cpp
+++ src/P2.cpp
@@ -227,14 +227,14 @@ T P2(T x, int64_t y, int threads)
 
 namespace primecount {
 
-int64_t P2(uint64_t x, int64_t y, int threads)
+int64_t P2(int64_t x, int64_t y, int threads)
 {
   return P2::P2(x, y, threads);
 }
 
 #ifdef HAVE_INT128_T
 
-int128_t P2(uint128_t x, int64_t y, int threads)
+int128_t P2(int128_t x, int64_t y, int threads)
 {
   return P2::P2(x, y, threads);
 }
