866e334d88b1a70b55ab48d8746c9a0a94577198
Kim Walisch <kim.walisch@gmail.com>
2014-12-26 23:15:44 +0100

    Speed improvement for 128-bit integers
    x2 < 2^64 in most cases, this means that x2 / primes[l] will use 64-bit division. Old code used x / (prime * primes[l]) which uses slow 128-bit division.

--- src/deleglise-rivat/S2_easy.cpp
+++ src/deleglise-rivat/S2_easy.cpp
@@ -53,38 +53,39 @@ T1 S2_easy(T1 x,
 
   int64_t pi_sqrty = pi[isqrt(y)];
   int64_t pi_x13 = pi[iroot<3>(x)];
-  T1 S2_total = 0;
+  T1 s2_easy = 0;
   double time = get_wtime();
   S2Status status;
 
-  #pragma omp parallel for schedule(dynamic, 1) num_threads(threads) reduction(+: S2_total)
+  #pragma omp parallel for schedule(dynamic, 1) num_threads(threads) reduction(+: s2_easy)
   for (int64_t b = max(c, pi_sqrty) + 1; b <= pi_x13; b++)
   {
-    T1 prime = primes[b];
-    int64_t prime64 =  primes[b];
+    int64_t prime = primes[b];
     int64_t min_trivial_leaf = min(x / (prime * prime), y);
-    int64_t min_clustered_easy_leaf = (int64_t) isqrt(x / prime64);
-    int64_t min_sparse_easy_leaf = z / prime64;
-    int64_t min_hard_leaf = max(y / prime64, prime64);
+    int64_t min_clustered_easy_leaf = (int64_t) isqrt(x / prime);
+    int64_t min_sparse_easy_leaf = z / prime;
+    int64_t min_hard_leaf = max(y / prime, prime);
 
     min_sparse_easy_leaf = max(min_sparse_easy_leaf, min_hard_leaf);
     min_clustered_easy_leaf = max(min_clustered_easy_leaf, min_hard_leaf);
     int64_t l = pi[min_trivial_leaf];
-    T1 S2_result = 0;
+
+    T1 result = 0;
+    T1 x2 = x / prime;
 
     // Find all clustered easy leaves:
     // x / n <= y and phi(x / n, b - 1) == phi(x / m, b - 1)
     // where phi(x / n, b - 1) = pi(x / n) - b + 2
     while (primes[l] > min_clustered_easy_leaf)
     {
-      T1 n = prime * primes[l];
-      int64_t xn = (int64_t) (x / n);
+      int64_t xn = (int64_t) (x2 / primes[l]);
+      assert(xn == x / (prime * primes[l]));
       int64_t phi_xn = pi[xn] - b + 2;
-      T1 m = prime * primes[b + phi_xn - 1];
-      int64_t xm = max((int64_t) (x / m), min_clustered_easy_leaf);
+      int64_t last_prime = primes[b + phi_xn - 1];
+      assert(x2 / last_prime == x / (prime * last_prime));
+      int64_t xm = max((int64_t) (x2 / last_prime), min_clustered_easy_leaf);
       int64_t l2 = pi[xm];
-      T1 phi_factor = l - l2;
-      S2_result += phi_xn * phi_factor;
+      result += phi_xn * (l - l2);
       l = l2;
     }
 
@@ -92,21 +93,21 @@ T1 S2_easy(T1 x,
     // x / n <= y and phi(x / n, b - 1) = pi(x / n) - b + 2
     for (; primes[l] > min_sparse_easy_leaf; l--)
     {
-      T1 n = prime * primes[l];
-      int64_t xn = (int64_t) (x / n);
-      S2_result += pi[xn] - b + 2;
+      int64_t xn = (int64_t) (x2 / primes[l]);
+      assert(xn == x / (prime * primes[l]));
+      result += pi[xn] - b + 2;
     }
 
     if (print_status())
       status.print(b, pi_x13);
 
-    S2_total += S2_result;
+    s2_easy += result;
   }
 
   if (print_status())
-    print_result("S2_easy", S2_total, time);
+    print_result("S2_easy", s2_easy, time);
 
-  return S2_total;
+  return s2_easy;
 }
 
 } // namespace S2_easy
