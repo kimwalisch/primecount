04058e0f7f3fa5459f27f653cd176a2a069b88a5
Kim Walisch <kim.walisch@gmail.com>
2014-12-29 23:12:17 +0100

    10% speed improvement for 128-bit integers
    Use strategy pattern to avoid 128-bit divisions if possible

--- src/deleglise-rivat/S2_easy.cpp
+++ src/deleglise-rivat/S2_easy.cpp
@@ -32,6 +32,75 @@ using namespace primecount;
 namespace {
 namespace S2_easy {
 
+/// Calculate the contribution of the clustered easy leaves and
+/// sparse easy leaves corresponding to primes[b] i.e.:
+/// n = primes[b] * primes[l] with x / n <= y.
+/// @param x2 = x / primes[b].
+///
+template <typename T1, typename T2, typename T3>
+T1 S2_easy(T1 x2,
+           int64_t y,
+           int64_t z,
+           int64_t b,
+           T2& pi,
+           vector<T3>& primes)
+{
+  T1 s2_easy = 0;
+  int64_t prime = primes[b];
+  int64_t min_trivial_leaf = min(x2 / prime, y);
+  int64_t min_clustered_easy_leaf = (int64_t) isqrt(x2);
+  int64_t min_sparse_easy_leaf = z / prime;
+  int64_t min_hard_leaf = max(y / prime, prime);
+
+  min_sparse_easy_leaf = max(min_sparse_easy_leaf, min_hard_leaf);
+  min_clustered_easy_leaf = max(min_clustered_easy_leaf, min_hard_leaf);
+  int64_t l = pi[min_trivial_leaf];
+
+  // Find all clustered easy leaves:
+  // n = primes[b] * primes[l]
+  // x / n <= y && phi(x / n, b - 1) == phi(x / m, b - 1)
+  // where phi(x / n, b - 1) = pi(x / n) - b + 2
+  while (primes[l] > min_clustered_easy_leaf)
+  {
+    int64_t xn = (int64_t) (x2 / primes[l]);
+    int64_t phi_xn = pi[xn] - b + 2;
+    int64_t last_prime = primes[b + phi_xn - 1];
+    int64_t xm = max((int64_t) (x2 / last_prime), min_clustered_easy_leaf);
+    int64_t l2 = pi[xm];
+    s2_easy += phi_xn * (l - l2);
+    l = l2;
+  }
+
+  // Find all sparse easy leaves:
+  // n = primes[b] * primes[l]
+  // x / n <= y && phi(x / n, b - 1) = pi(x / n) - b + 2
+  for (; primes[l] > min_sparse_easy_leaf; l--)
+  {
+    int64_t xn = (int64_t) (x2 / primes[l]);
+    s2_easy += pi[xn] - b + 2;
+  }
+
+  return s2_easy;
+}
+
+/// Strategy pattern, chooses an S2_easy() implementation.
+template <typename T1, typename T2, typename T3>
+T1 strategy(T1 x,
+            int64_t y,
+            int64_t z,
+            int64_t b,
+            T2& pi,
+            vector<T3>& primes)
+{
+  T1 x2 = x / primes[b];
+
+  // Avoid slow 128-bit divisions
+  if (x2 <= std::numeric_limits<uint64_t>::max())
+    return S2_easy((uint64_t) x2, y, z, b, pi, primes);
+
+  return S2_easy(x2, y, z, b, pi, primes);
+}
+
 /// Calculate the contribution of the clustered easy leaves
 /// and the sparse easy leaves.
 /// @param T  either int64_t or uint128_t.
@@ -61,47 +130,10 @@ T1 S2_easy(T1 x,
   #pragma omp parallel for schedule(dynamic, 1) num_threads(threads) reduction(+: s2_easy)
   for (int64_t b = max(c, pi_sqrty) + 1; b <= pi_x13; b++)
   {
-    int64_t prime = primes[b];
-    int64_t min_trivial_leaf = min(x / (prime * prime), y);
-    int64_t min_clustered_easy_leaf = (int64_t) isqrt(x / prime);
-    int64_t min_sparse_easy_leaf = z / prime;
-    int64_t min_hard_leaf = max(y / prime, prime);
-
-    min_sparse_easy_leaf = max(min_sparse_easy_leaf, min_hard_leaf);
-    min_clustered_easy_leaf = max(min_clustered_easy_leaf, min_hard_leaf);
-    int64_t l = pi[min_trivial_leaf];
-
-    T1 result = 0;
-    T1 x2 = x / prime;
-
-    // Find all clustered easy leaves:
-    // n = primes[b] * primes[l]
-    // x / n <= y && phi(x / n, b - 1) == phi(x / m, b - 1)
-    // where phi(x / n, b - 1) = pi(x / n) - b + 2
-    while (primes[l] > min_clustered_easy_leaf)
-    {
-      int64_t xn = (int64_t) (x2 / primes[l]);
-      int64_t phi_xn = pi[xn] - b + 2;
-      int64_t last_prime = primes[b + phi_xn - 1];
-      int64_t xm = max((int64_t) (x2 / last_prime), min_clustered_easy_leaf);
-      int64_t l2 = pi[xm];
-      result += phi_xn * (l - l2);
-      l = l2;
-    }
-
-    // Find all sparse easy leaves:
-    // n = primes[b] * primes[l]
-    // x / n <= y && phi(x / n, b - 1) = pi(x / n) - b + 2
-    for (; primes[l] > min_sparse_easy_leaf; l--)
-    {
-      int64_t xn = (int64_t) (x2 / primes[l]);
-      result += pi[xn] - b + 2;
-    }
+    s2_easy += strategy(x, y, z, b, pi, primes);
 
     if (print_status())
       status.print(b, pi_x13);
-
-    s2_easy += result;
   }
 
   if (print_status())
