e3f5b60a93a2c511350bbe0cbffd1a86badab818
Kim Walisch <kim.walisch@gmail.com>
2015-01-03 18:57:11 +0100

    Comment

--- include/BitSieve.hpp
+++ include/BitSieve.hpp
@@ -32,7 +32,7 @@ public:
   /// Count the number of 1 bits inside the interval [start, stop]
   uint64_t count(uint64_t start, uint64_t stop) const;
 
-  /// Count the number of 1 bits inside the interval [0, stop]
+  /// Count the number of 1 bits inside [0, stop]
   uint64_t count(uint64_t stop) const;
 
   bool operator[](uint64_t pos) const
