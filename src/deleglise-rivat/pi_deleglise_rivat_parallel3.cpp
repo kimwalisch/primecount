99ece5187cb93df46deb398b88779f9f4f268aaf
Kim Walisch <kim.walisch@gmail.com>
2015-03-03 21:30:11 +0100

    alpha factor tuning

--- src/deleglise-rivat/pi_deleglise_rivat_parallel3.cpp
+++ src/deleglise-rivat/pi_deleglise_rivat_parallel3.cpp
@@ -64,7 +64,7 @@ int128_t pi_deleglise_rivat_parallel3(int128_t x, int threads)
   if (x > to_maxint(primecount::max()))
     throw primecount_error("pi(x): x must be <= " + max());
 
-  double alpha = get_alpha(x, 0.00162193, -0.0450437, 0.359918, 0.482142);
+  double alpha = get_alpha(x, 0.00142219, -0.032211, 0.227464, 0.185949);
   int64_t x13 = iroot<3>(x);
   int64_t y = (int64_t) (x13 * alpha);
   int64_t z = (int64_t) (x / y);
