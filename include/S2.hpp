5726598476c70890dfd080c3abdf124e59dc6a98
Kim Walisch <kim.walisch@gmail.com>
2016-02-25 18:05:49 +0100

    Add MPI functions

--- include/S2.hpp
+++ include/S2.hpp
@@ -52,6 +52,26 @@ int128_t S2_easy(int128_t x,
 
 #endif
 
+#ifdef HAVE_MPI
+
+int64_t S2_easy_mpi(int64_t x,
+                    int64_t y,
+                    int64_t z,
+                    int64_t c,
+                    int threads);
+
+#ifdef HAVE_INT128_T
+
+int128_t S2_easy_mpi(int128_t x,
+                     int64_t y,
+                     int64_t z,
+                     int64_t c,
+                     int threads);
+
+#endif
+
+#endif
+
 /// ------------------------ S2_hard() -------------------------------
 
 int64_t S2_hard(int64_t x,
@@ -69,6 +89,28 @@ int128_t S2_hard(int128_t x,
                  int64_t c,
                  int128_t s2_hard_approx,
                  int threads);
+
+#endif
+
+#ifdef HAVE_MPI
+
+int64_t S2_hard_mpi(int64_t x,
+                    int64_t y,
+                    int64_t z,
+                    int64_t c,
+                    int64_t s2_hard_approx,
+                    int threads);
+
+#ifdef HAVE_INT128_T
+
+int128_t S2_hard_mpi(int128_t x,
+                     int64_t y,
+                     int64_t z,
+                     int64_t c,
+                     int128_t s2_hard_approx,
+                     int threads);
+
+#endif
 
 #endif
 
