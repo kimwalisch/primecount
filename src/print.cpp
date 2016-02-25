17e45a943b83ec21bd5fcebf0d47cf577b785d4f
Kim Walisch <kim.walisch@gmail.com>
2016-02-25 22:16:53 +0100

    Add MPI support

--- src/print.cpp
+++ src/print.cpp
@@ -20,8 +20,6 @@ using namespace std;
 
 namespace {
 
-bool print_result_ = true;
-
 bool print_status_ = false;
 
 bool print_variables_ = false;
@@ -32,21 +30,39 @@ namespace primecount {
 
 void set_print_status(bool print_status)
 {
+#ifdef HAVE_MPI
+  print_status_ = print_status && is_mpi_master_proc();
+#else
   print_status_ = print_status;
+#endif
 }
 
 void set_print_variables(bool print_variables)
 {
+#ifdef HAVE_MPI
+  print_variables_ = print_variables && is_mpi_master_proc();
+#else
   print_variables_ = print_variables;
-
-  // Avoid printing the result twice in standalone mode
-  // when e.g. only computing S2_hard(x, y)
-  print_result_ = !print_variables;
+#endif
 }
 
 bool print_result()
 {
-  return print_result_;
+#ifdef HAVE_MPI
+  return !print_variables() && is_mpi_master_proc();
+#else
+  return !print_variables();
+#endif
+}
+
+void print_threads(int threads)
+{
+#ifdef HAVE_MPI
+  cout << "processes = " << mpi_num_procs() << endl;
+  cout << "threads = " << mpi_num_procs() << " * " << validate_threads(threads) << endl;
+#else
+  cout << "threads = " << validate_threads(threads) << endl;
+#endif
 }
 
 bool print_status()
@@ -67,37 +83,34 @@ void print(const string& str)
 
 void print(maxint_t x, int64_t y, int64_t z, int64_t c, double alpha, int threads)
 {
-  if (print_status() &&
-      print_variables())
+  if (print_variables())
   {
     cout << "x = " << x << endl;
     cout << "y = " << y << endl;
     cout << "z = " << z << endl;
     cout << "c = " << c << endl;
     cout << "alpha = " << fixed << setprecision(3) << alpha << endl;
-    cout << "threads = " << validate_threads(threads) << endl;
+    print_threads(threads);
   }
 }
 
 void print(maxint_t x, int64_t y, int threads)
 {
-  if (print_status() &&
-      print_variables())
+  if (print_variables())
   {
     maxint_t z = x / y;
     cout << "x = " << x << endl;
     cout << "y = " << y << endl;
     cout << "z = " << z << endl;
     cout << "alpha = " << fixed << setprecision(3) << get_alpha(x, y) << endl;
-    cout << "threads = " << validate_threads(threads) << endl;
+    print_threads(threads);
     cout << endl;
   }
 }
 
 void print(maxint_t x, int64_t y, int64_t c, int threads)
 {
-  if (print_status() &&
-      print_variables())
+  if (print_variables())
   {
     maxint_t z = x / y;
     cout << "x = " << x << endl;
@@ -105,18 +118,18 @@ void print(maxint_t x, int64_t y, int64_t c, int threads)
     cout << "z = " << z << endl;
     cout << "c = " << c << endl;
     cout << "alpha = " << fixed << setprecision(3) << get_alpha(x, y) << endl;
-    cout << "threads = " << validate_threads(threads) << endl;
+    print_threads(threads);
     cout << endl;
   }
 }
 
-void print(const string& res_name, maxint_t res, double time)
+void print(const string& res_str, maxint_t res, double time)
 {
   if (print_status())
   {
     cout << "\r" << string(50,' ') << "\r";
     cout << "Status: 100%" << endl;
-    cout << res_name << " = " << res << endl;
+    cout << res_str << " = " << res << endl;
     print_seconds(get_wtime() - time);
   }
 }
