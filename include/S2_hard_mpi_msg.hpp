53d08c1a90039deea2d53a10076f6a6b52511ed8
Kim Walisch <kim.walisch@gmail.com>
2016-03-07 11:02:30 +0100

    Add init_seconds

--- include/S2_hard_mpi_msg.hpp
+++ include/S2_hard_mpi_msg.hpp
@@ -42,6 +42,7 @@ public:
   int64_t high() const;
   int64_t segment_size() const;
   int64_t segments_per_thread() const;
+  double init_seconds() const;
   double seconds() const;
   double rsd() const;
   bool finished() const;
@@ -59,6 +60,7 @@ public:
                   int64_t segment_size,
                   int64_t segments_per_thread,
                   T s2_hard,
+                  double init_seconds,
                   double seconds,
                   double rsd)
   {
@@ -70,6 +72,7 @@ public:
     msgData_.segment_size = segment_size;
     msgData_.segments_per_thread = segments_per_thread;
     *((T*) msgData_.s2_hard) = s2_hard;
+    msgData_.init_seconds = init_seconds;
     msgData_.seconds = seconds;
     msgData_.rsd = rsd;
     msgData_.finished = false;
@@ -90,6 +93,7 @@ private:
     int64_t segment_size;
     int64_t segments_per_thread;
     int64_t s2_hard[2];
+    double init_seconds;
     double seconds;
     double rsd;
     int finished;
