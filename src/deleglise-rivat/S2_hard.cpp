efb782ab249f48ee5688fb7f14f3493c55284250
Kim Walisch <kim.walisch@gmail.com>
2016-03-11 17:50:24 +0100

    Remove useless optimization

--- src/deleglise-rivat/S2_hard.cpp
+++ src/deleglise-rivat/S2_hard.cpp
@@ -356,11 +356,6 @@ T S2_hard_OpenMP_master(T x,
 
   while (low < limit)
   {
-    // make sure we use all CPU cores
-    segment_size = min(segment_size, (limit - low) / threads);
-    segment_size = max(segment_size, min_segment_size);
-    segment_size = prev_power_of_2(segment_size);
-
     int64_t segments = ceil_div(limit - low, segment_size);
     threads = in_between(1, threads, segments);
     segments_per_thread = in_between(1, segments_per_thread, ceil_div(segments, threads));
