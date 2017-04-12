5856da5094bb3b848f576d30e4028b8cc62de252
Kim Walisch <kim.walisch@gmail.com>
2017-04-12 13:12:45 +0200

    Include <primecount.hpp> first

--- src/test.cpp
+++ src/test.cpp
@@ -8,8 +8,8 @@
 /// file in the top level directory.
 ///
 
-#include <primecount-internal.hpp>
 #include <primecount.hpp>
+#include <primecount-internal.hpp>
 #include <primesieve.hpp>
 #include <int128_t.hpp>
 
