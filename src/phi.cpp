c9403f6e90d90bee1debd2012baffac1c7c1e18a
Kim Walisch <kim.walisch@gmail.com>
2017-04-14 15:09:07 +0200

    Rename is_phi_by_pix() to is_pix()

--- src/phi.cpp
+++ src/phi.cpp
@@ -83,7 +83,7 @@ public:
       sum = SIGN;
     else if (is_phi_tiny(a))
       sum = phi_tiny(x, a) * SIGN;
-    else if (is_phi_by_pix(x, a))
+    else if (is_pix(x, a))
       sum = (pi_[x] - a + 1) * SIGN;
     else
     {
@@ -117,7 +117,7 @@ public:
       }
     }
 
-    if (write_to_cache(x, a))
+    if (update_cache(x, a))
       cache_[a][x] = (uint16_t) (sum * SIGN);
 
     return sum;
@@ -134,7 +134,7 @@ private:
     return (int64_t) cache_[a].size();
   }
 
-  bool is_phi_by_pix(int64_t x, int64_t a) const
+  bool is_pix(int64_t x, int64_t a) const
   {
     return x < pi_.size() &&
            x < isquare(primes_[a + 1]);
@@ -147,10 +147,9 @@ private:
            cache_[a][x] != 0;
   }
 
-  bool write_to_cache(int64_t x, int64_t a)
+  bool update_cache(int64_t x, int64_t a)
   {
-    if (a > MAX_A || 
-        x > numeric_limits<uint16_t>::max())
+    if (a > MAX_A || x > numeric_limits<uint16_t>::max())
       return false;
 
     // we need to increase cache size
