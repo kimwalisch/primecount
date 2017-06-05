8ea31582779390ff533943570e86ca98c985c025
Kim Walisch <kim.walisch@gmail.com>
2017-06-05 21:12:01 +0200

    Add S1 test

---
+++ test/S1_xy.cpp
@@ -0,0 +1,64 @@
+///
+/// @file   S1_xy.cpp
+/// @brief  Test the computation of the ordinary leaves
+///         S1(x, y) used in the Lagarias-Miller-Odlyzko
+///         and Deleglise-Rivat prime counting algorithms.
+///
+/// Copyright (C) 2017 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#include <S1.hpp>
+#include <generate.hpp>
+#include <PhiTiny.hpp>
+#include <imath.hpp>
+
+#include <stdint.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <random>
+
+using namespace std;
+using namespace primecount;
+
+void check(bool OK)
+{
+  cout << "   " << (OK ? "OK" : "ERROR") << "\n";
+  if (!OK)
+    exit(1);
+}
+
+int main()
+{
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> dist(0, 10000000);
+
+  for (int i = 0; i < 1000; i++)
+  {
+    int64_t x = dist(gen);
+    int64_t y = iroot<3>(x);
+    int64_t c = PhiTiny::get_c(y);
+    int64_t s1 = 0;
+
+    auto primes = generate_n_primes<int32_t>(c);
+    auto lpf = generate_lpf(y);
+    auto mu = generate_moebius(y);
+
+    // ordinary leaves
+    for (int64_t n = 1; n <= y; n++)
+      if (lpf[n] > primes[c])
+        s1 += mu[n] * phi_tiny(x / n, c);
+
+    cout << "S1(" << x << ", " << y << ") = " << s1;
+    check(s1 == S1(x, y, c, 1));
+  }
+
+  cout << endl;
+  cout << "All tests passed successfully!" << endl;
+
+  return 0;
+}
