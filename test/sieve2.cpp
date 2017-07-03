e96e6c74e6d6dc8adf5743a8cff0f0de093cef58
Kim Walisch <kim.walisch@gmail.com>
2017-07-03 17:14:49 +0200

    Get rid of BitSieve

---
+++ test/sieve2.cpp
@@ -0,0 +1,75 @@
+///
+/// @file   sieve2.cpp
+/// @brief  Test the return value of Sieve::cross_off(prime)
+///         which returns the number of multiples of prime
+///         that have been crossed off for the first time in
+///         the sieve array.
+///
+/// Copyright (C) 2017 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#include <Sieve.hpp>
+#include <generate.hpp>
+#include <imath.hpp>
+
+#include <stdint.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <random>
+
+using namespace std;
+using namespace primecount;
+
+void check(bool OK)
+{
+  cout << "   " << (OK ? "OK" : "ERROR") << "\n";
+  if (!OK)
+    exit(1);
+}
+
+int main()
+{
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> dist(1000000, 2000000);
+
+  int low = 0;
+  int high = dist(gen);
+  auto primes = generate_primes<int>(isqrt(high));
+
+  Sieve sieve(low, high, primes.size());
+  vector<int> sieve2(high, 1);
+  sieve2[0] = 0;
+
+  for (size_t i = 1; i < primes.size(); i++)
+  {
+    int cnt1 = 0;
+    int cnt2 = 0;
+
+    if (primes[i] <= 5)
+      sieve.pre_sieve(i, low, high);
+    else
+      cnt1 = sieve.cross_off(i, primes[i]);
+
+    for (int j = primes[i]; j < high; j += primes[i])
+    {
+      cnt2 += sieve2[j];
+      sieve2[j] = 0;
+    }
+
+    if (primes[i] > 5)
+    {
+      cout << "sieve.cross_off(" << i << ", " << primes[i] << ") = " << cnt1;
+      check(cnt1 == cnt2);
+    }
+  }
+
+  cout << endl;
+  cout << "All tests passed successfully!" << endl;
+
+  return 0;
+}
