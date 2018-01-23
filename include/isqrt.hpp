21f6a567816b9c785b1d93df03bfe529b92a03ad
Kim Walisch <kim.walisch@gmail.com>
2018-01-23 20:54:40 +0100

    MSVC does not have int128_t

--- include/isqrt.hpp
+++ include/isqrt.hpp
@@ -21,35 +21,49 @@ namespace primecount {
 template <typename T>
 struct isqrt_limits { };
 
-template <> struct isqrt_limits<int8_t>
-{ static constexpr int8_t max() { return 11; } };
+template <> struct isqrt_limits<int8_t> {
+  static constexpr int8_t max() { return 11; }
+};
 
-template <> struct isqrt_limits<uint8_t>
-{ static constexpr uint8_t max() { return 15; } };
+template <> struct isqrt_limits<uint8_t> {
+  static constexpr uint8_t max() { return 15; }
+};
 
-template <> struct isqrt_limits<int16_t>
-{ static constexpr int16_t max() { return 181; } };
+template <> struct isqrt_limits<int16_t> {
+  static constexpr int16_t max() { return 181; }
+};
 
-template <> struct isqrt_limits<uint16_t>
-{ static constexpr uint16_t max() { return 255; } };
+template <> struct isqrt_limits<uint16_t> {
+  static constexpr uint16_t max() { return 255; }
+};
 
-template <> struct isqrt_limits<int32_t>
-{ static constexpr int32_t max() { return 46340; } };
+template <> struct isqrt_limits<int32_t> {
+  static constexpr int32_t max() { return 46340; }
+};
 
-template <> struct isqrt_limits<uint32_t>
-{ static constexpr uint32_t max() { return 65535; } };
+template <> struct isqrt_limits<uint32_t> {
+  static constexpr uint32_t max() { return 65535; }
+};
 
-template <> struct isqrt_limits<int64_t>
-{ static constexpr int64_t max() { return 3037000499ll; } };
+template <> struct isqrt_limits<int64_t> {
+  static constexpr int64_t max() { return 3037000499ll; }
+};
 
-template <> struct isqrt_limits<uint64_t>
-{ static constexpr uint64_t max() { return 4294967295ull; } };
+template <> struct isqrt_limits<uint64_t> {
+  static constexpr uint64_t max() { return 4294967295ull; }
+};
 
-template <> struct isqrt_limits<int128_t>
-{ static constexpr uint64_t max() { return 13043817825332782212ull; } };
+#if defined(HAVE_INT128_T)
 
-template <> struct isqrt_limits<uint128_t>
-{ static constexpr uint64_t max() { return 18446744073709551615ull; } };
+template <> struct isqrt_limits<int128_t> {
+  static constexpr uint64_t max() { return 13043817825332782212ull; }
+};
+
+template <> struct isqrt_limits<uint128_t> {
+  static constexpr uint64_t max() { return 18446744073709551615ull; }
+};
+
+#endif
 
 template <typename T>
 inline T isqrt(T x)
