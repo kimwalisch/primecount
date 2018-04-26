3b63bfb61f4bf8e10e390e782007da38e03ffb91
Kim Walisch <kim.walisch@gmail.com>
2018-04-26 19:44:12 +0200

    Update version to 4.4

--- include/primecount.hpp
+++ include/primecount.hpp
@@ -14,9 +14,9 @@
 #include <string>
 #include <stdint.h>
 
-#define PRIMECOUNT_VERSION "4.3"
+#define PRIMECOUNT_VERSION "4.4"
 #define PRIMECOUNT_VERSION_MAJOR 4
-#define PRIMECOUNT_VERSION_MINOR 3
+#define PRIMECOUNT_VERSION_MINOR 4
 
 namespace primecount {
 
