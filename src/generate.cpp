71f7c0022c46c2397c21fe24ce851ced78465155
kimwalisch <kim.walisch@gmail.com>
2018-05-04 10:58:27 +0200

    Fix 32-bit integer overflows

--- src/generate.cpp
+++ src/generate.cpp
@@ -8,6 +8,7 @@
 ///
 
 #include <generate.hpp>
+#include <isqrt.hpp>
 
 #include <stdint.h>
 #include <limits>
@@ -17,26 +18,54 @@ using namespace std;
 
 namespace primecount {
 
+/// Generate a vector with the prime counts <= max
+/// using the sieve of Eratosthenes
+///
+vector<int32_t> generate_pi(int64_t max)
+{
+  int64_t sqrt = isqrt(max);
+  int64_t size = max + 1;
+  vector<char> sieve(size, 1);
+
+  for (int64_t i = 2; i <= sqrt; i++)
+    if (sieve[i])
+      for (int64_t j = i * i; j < size; j += i)
+        sieve[j] = 0;
+
+  vector<int32_t> pi(size, 0);
+  int32_t pix = 0;
+
+  for (int64_t i = 2; i < size; i++)
+  {
+    pix += sieve[i];
+    pi[i] = pix;
+  }
+
+  return pi;
+}
+
 /// Generate a vector with Möbius function values.
 /// This implementation is based on code by Rick Sladkey:
 /// https://mathoverflow.net/q/99545
 ///
 vector<int32_t> generate_moebius(int64_t max)
 {
-  vector<int32_t> mu(max + 1, 1);
+  int64_t sqrt = isqrt(max);
+  int64_t size = max + 1;
+  vector<int32_t> mu(size, 1);
 
-  for (int32_t i = 2; i * i <= max; i++)
+  for (int64_t i = 2; i <= sqrt; i++)
   {
     if (mu[i] == 1)
     {
-      for (int32_t j = i; j <= max; j += i)
-        mu[j] *= -i;
-      for (int32_t j = i * i; j <= max; j += i * i)
+      for (int64_t j = i; j < size; j += i)
+        mu[j] *= (int32_t) -i;
+      for (int64_t j = i * i; j < size; j += i * i)
         mu[j] = 0;
     }
   }
 
-  for (int32_t i = 2; i <= max; i++)
+  for (int64_t i = 2; i < size; i++)
   {
     if (mu[i] == i)
       mu[i] = 1;
@@ -56,7 +85,19 @@ vector<int32_t> generate_moebius(int64_t max)
 ///
 vector<int32_t> generate_lpf(int64_t max)
 {
-  vector<int32_t> lpf(max + 1, 1);
+  int64_t sqrt = isqrt(max);
+  int64_t size = max + 1;
+  vector<int32_t> lpf(size, 1);
+
+  for (int64_t i = 2; i <= sqrt; i++)
+    if (lpf[i] == 1)
+      for (int64_t j = i * i; j < size; j += i)
+        if (lpf[j] == 1)
+          lpf[j] = (int32_t) i;
+
+  for (int64_t i = 2; i < size; i++)
+    if (lpf[i] == 1)
+      lpf[i] = (int32_t) i;
 
   // phi(x / 1, c) contributes to the sum in the
   // Lagarias-Miller-Odlyzko prime counting algorithm,
@@ -64,42 +105,7 @@ vector<int32_t> generate_lpf(int64_t max)
   if (lpf.size() > 1)
     lpf[1] = numeric_limits<int32_t>::max();
 
-  for (int32_t i = 2; i * i <= max; i++)
-    if (lpf[i] == 1)
-      for (int32_t j = i * 2; j <= max; j += i)
-        if (lpf[j] == 1)
-          lpf[j] = i;
-
-  for (int32_t i = 2; i <= max; i++)
-    if (lpf[i] == 1)
-      lpf[i] = i;
-
   return lpf;
-}
-
-/// Generate a vector with the prime counts <= max
-/// using the sieve of Eratosthenes
-///
-vector<int32_t> generate_pi(int64_t max)
-{
-  int64_t size = max + 1;
-  vector<char> sieve(size, 1);
-
-  for (int64_t i = 2; i * i <= max; i++)
-    if (sieve[i])
-      for (int64_t j = i * i; j <= max; j += i)
-        sieve[j] = 0;
-
-  vector<int32_t> pi(size, 0);
-  int32_t pix = 0;
-
-  for (int64_t x = 2; x <= max; x++)
-  {
-    pix += sieve[x];
-    pi[x] = pix;
-  }
-
-  return pi;
 }
 
 } // namespace
