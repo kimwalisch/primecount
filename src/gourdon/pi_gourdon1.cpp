4f3a24936ad5b1d3909ea402789338fe45419a5d
kimwalisch <kim.walisch@gmail.com>
2019-07-20 16:12:36 +0200

    Add C(x, y) formula

--- src/gourdon/pi_gourdon1.cpp
+++ src/gourdon/pi_gourdon1.cpp
@@ -39,7 +39,7 @@ int64_t pi_gourdon1(int64_t x, int64_t y, int64_t z, int64_t k)
 
   int64_t a = A(x, y, 1);
   int64_t b = B(x, y, 1);
-  int64_t c = 0;
+  int64_t c = C(x, y, z, k, 1);
   int64_t d = 0;
   int64_t phi0 = Phi0(x, y, z, k, 1);
   int64_t sigma = Sigma(x, y, 1);
