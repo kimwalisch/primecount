51a4862af1cc582c245dd5ce7faced80a62411b8
kimwalisch <kim.walisch@gmail.com>
2019-07-21 16:25:08 +0200

    Create pi_gourdon.cpp

---
+++ src/gourdon/pi_gourdon.cpp
@@ -0,0 +1,43 @@
+///
+/// @file  pi_gourdon.cpp
+/// @brief Simple demonstration implementation of Xavier Gourdon's
+///        prime counting algorithm. Xavier Gourdon's algorithm is
+///        an improved version of the Deleglise-Rivat algorithm.
+///
+///        Xavier Gourdon formula:
+///        pi(x) = A - B + C + D + phi0 + Sigma
+///
+/// Copyright (C) 2019 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#include <gourdon.hpp>
+#include <stdint.h>
+
+namespace primecount {
+
+/// Calculate the number of primes below x using
+/// Xavier Gourdon's algorithm.
+/// Run time: O(x^(2/3) / (log x)^2)
+/// Memory usage: O(x^(1/3) * (log x)^3)
+///
+int64_t pi_gourdon(int64_t x, int64_t y, int64_t z, int64_t k)
+{
+  if (x < 2)
+    return 0;
+
+  int64_t a = A(x, y, 1);
+  int64_t b = B(x, y, 1);
+  int64_t c = C(x, y, z, k, 1);
+  int64_t d = D(x, y, z, k);
+  int64_t phi0 = Phi0(x, y, z, k, 1);
+  int64_t sigma = Sigma(x, y, 1);
+
+  int64_t sum = a - b + c + d + phi0 + sigma;
+
+  return sum;
+}
+
+} // namespace
