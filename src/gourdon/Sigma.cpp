f923dffaca7389762e77f8c95199a70efccd2e67
kimwalisch <kim.walisch@gmail.com>
2019-08-21 11:46:22 +0200

    Avoid duplicate initialization

--- src/gourdon/Sigma.cpp
+++ src/gourdon/Sigma.cpp
@@ -1,11 +1,10 @@
 ///
 /// @file  Sigma.cpp
 ///        The 7 sigma formulas are the least computationally
-///        expensive formulas in Gourdon's algorithm. The Sigma6
-///        formula has not been fully implemented yet, it uses
-///        O(x^(3/8)) instead of O(y). The Sigma5 and Sigma6 formulas
-///        could also be parallelized although this won't improve
-///        performance overall.
+///        expensive formulas in Gourdon's algorithm. Sigma0 has a
+///        runtime complexity of O(x(1/2)), all other formulas
+///        have a runtime complexity of O(y) and hence it does not
+///        make much sense to use multi-threading.
 ///
 /// Copyright (C) 2019 Kim Walisch, <kim.walisch@gmail.com>
 ///
@@ -31,78 +30,53 @@ using namespace primecount;
 namespace {
 
 template <typename T>
-T Sigma0(T x, int64_t y, int threads)
+T Sigma0(T x, T a, int threads)
 {
-  T a = pi_legendre(y, threads);
   T pi_sqrtx = pi_legendre(isqrt(x), threads);
-  T sum = a - 1 + (pi_sqrtx * (pi_sqrtx - 1)) / 2 - (a * (a - 1)) / 2;
-
-  return sum;
+  return a - 1 + (pi_sqrtx * (pi_sqrtx - 1)) / 2 - (a * (a - 1)) / 2;
 }
 
 template <typename T>
-T Sigma1(T x, int64_t y, int threads)
+T Sigma1(T a, T b)
 {
-  T a = pi_legendre(y, threads);
-  T b = pi_legendre(iroot<3>(x), threads);
-  T sum = (a - b) * (a - b - 1) / 2;
-
-  return sum;
+  return (a - b) * (a - b - 1) / 2;
 }
 
 template <typename T>
-T Sigma2(T x, int64_t y, int threads)
+T Sigma2(T a, T b, T c, T d)
 {
-  T a = pi_legendre(y, threads);
-  T b = pi_legendre(iroot<3>(x), threads);
-  T c = pi_legendre(isqrt(x / y), threads);
-  T x_star = get_x_star_gourdon(x, y);
-  T d = pi_legendre(x_star, threads);
-  T sum = a * (b - c - (c * (c - 3)) / 2 + (d * (d - 3)) / 2);
-
-  return sum;
+  return a * (b - c - (c * (c - 3)) / 2 + (d * (d - 3)) / 2);
 }
 
 template <typename T>
-T Sigma3(T x, int64_t y, int threads)
+T Sigma3(T b, T d)
 {
-  T b = pi_legendre(iroot<3>(x), threads);
-  T x_star = get_x_star_gourdon(x, y);
-  T d = pi_legendre(x_star, threads);
-  T sum = (b * (b - 1) * (2 * b - 1)) / 6 - b - (d * (d - 1) * (2 * d - 1)) / 6 + d;
-
-  return sum;
+  return (b * (b - 1) * (2 * b - 1)) / 6 - b - (d * (d - 1) * (2 * d - 1)) / 6 + d;
 }
 
 /// Memory usage: O(x^(1/3)) or less
 template <typename T>
-T Sigma4(T x, int64_t y, int threads)
+T Sigma4(T x, int64_t y, int64_t a, int64_t x_star, const PiTable& pi)
 {
   T sum = 0;
-  int64_t pi_y = pi_legendre(y, threads);
-  int64_t x_star = get_x_star_gourdon(x, y);
   int64_t sqrt_xy = isqrt(x / y);
-
-  PiTable pi(x / (x_star * (T) y));
   primesieve::iterator it(x_star, sqrt_xy);
   int64_t prime = it.next_prime();
 
   for (; prime <= sqrt_xy; prime = it.next_prime())
     sum += pi[x / (prime * (T) y)];
 
-  sum *= pi_y;
+  sum *= a;
   return sum;
 }
 
 /// Memory usage: O(y)
 template <typename T>
-T Sigma5(T x, int64_t y)
+T Sigma5(T x, int64_t y, const PiTable& pi)
 {
   T sum = 0;
   int64_t x13 = iroot<3>(x);
   int64_t sqrt_xy = isqrt(x / y);
-
-  PiTable pi(y);
   primesieve::iterator it(sqrt_xy, x13);
   int64_t prime = it.next_prime();
 
@@ -114,13 +88,10 @@ T Sigma5(T x, int64_t y)
 
 /// Memory usage: O(x^(3/8))
 template <typename T>
-T Sigma6(T x, int64_t y)
+T Sigma6(T x, int64_t x_star, const PiTable& pi)
 {
   T sum = 0;
   int64_t x13 = iroot<3>(x);
-  int64_t x_star = get_x_star_gourdon(x, y);
-
-  PiTable pi(isqrt(x / x_star));
   primesieve::iterator it(x_star, x13);
   int64_t prime = it.next_prime();
 
@@ -148,14 +119,26 @@ int64_t Sigma(int64_t x, int64_t y, int threads)
   print("=== Sigma(x, y) ===");
   print(x, y, threads);
 
+  int64_t x_star = get_x_star_gourdon(x, y);
+  int64_t a = pi_legendre(y, threads);
+  int64_t b = pi_legendre(iroot<3>(x), threads);
+  int64_t c = pi_legendre(isqrt(x / y), threads);
+  int64_t d = pi_legendre(x_star, threads);
+
+  int64_t max_pix_sigma4 = x / (x_star * y);
+  int64_t max_pix_sigma5 = y;
+  int64_t max_pix_sigma6 = isqrt(x / x_star);
+  int64_t max_pix = max(max_pix_sigma4, max_pix_sigma5, max_pix_sigma6);
+  PiTable pi(max_pix);
+
   double time = get_time();
-  int64_t sum = Sigma0(x, y, threads) +
-                Sigma1(x, y, threads) +
-                Sigma2(x, y, threads) +
-                Sigma3(x, y, threads) +
-                Sigma4(x, y, threads) +
-                Sigma5(x, y) +
-                Sigma6(x, y);
+  int64_t sum = Sigma0(x, a, threads) +
+                Sigma1(a, b) +
+                Sigma2(a, b, c, d) +
+                Sigma3(b, d) +
+                Sigma4(x, y, a, x_star, pi) +
+                Sigma5(x, y, pi) +
+                Sigma6(x, x_star, pi);
   
   print("Sigma", sum, time);
   return sum;
@@ -169,14 +152,26 @@ int128_t Sigma(int128_t x, int64_t y, int threads)
   print("=== Sigma(x, y) ===");
   print(x, y, threads);
 
+  int128_t x_star = get_x_star_gourdon(x, y);
+  int128_t a = pi_legendre(y, threads);
+  int128_t b = pi_legendre(iroot<3>(x), threads);
+  int128_t c = pi_legendre(isqrt(x / y), threads);
+  int128_t d = pi_legendre(x_star, threads);
+
+  int64_t max_pix_sigma4 = x / (x_star * y);
+  int64_t max_pix_sigma5 = y;
+  int64_t max_pix_sigma6 = isqrt(x / x_star);
+  int64_t max_pix = max(max_pix_sigma4, max_pix_sigma5, max_pix_sigma6);
+  PiTable pi(max_pix);
+
   double time = get_time();
-  int128_t sum = Sigma0(x, y, threads) +
-                 Sigma1(x, y, threads) +
-                 Sigma2(x, y, threads) +
-                 Sigma3(x, y, threads) +
-                 Sigma4(x, y, threads) +
-                 Sigma5(x, y) +
-                 Sigma6(x, y);
+  int128_t sum = Sigma0(x, a, threads) +
+                 Sigma1(a, b) +
+                 Sigma2(a, b, c, d) +
+                 Sigma3(b, d) +
+                 Sigma4(x, y, a, x_star, pi) +
+                 Sigma5(x, y, pi) +
+                 Sigma6(x, x_star, pi);
 
   print("Sigma", sum, time);
   return sum;
