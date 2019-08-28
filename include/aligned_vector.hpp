06563a6571514c16aeefc9e2955e00adf96645c2
kimwalisch <kim.walisch@gmail.com>
2019-08-28 17:10:16 +0200

    Increase maximum cache line size

--- include/aligned_vector.hpp
+++ include/aligned_vector.hpp
@@ -1,7 +1,7 @@
 ///
 /// @file  aligned_vector.hpp
 ///
-/// Copyright (C) 2017 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2019 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
@@ -13,9 +13,14 @@
 #include <cstddef>
 #include <vector>
 
-// Maximum cache line size of current CPUs
+/// Maximum cache line size of current CPUs.
+/// Note that in 2019 all x86 CPU have a cache line size of 64 bytes.
+/// However there are CPUs out there that have much larger cache line
+/// sizes e.g. IBM z13 CPUs from 2015 have a cache line size of 256
+/// bytes. Hence in order to be future-proof we set the maximum cache
+/// line size to 1 kilobyte.
 #ifndef CACHE_LINE_SIZE
-  #define CACHE_LINE_SIZE 512
+  #define CACHE_LINE_SIZE 1024
 #endif
 
 namespace primecount {
