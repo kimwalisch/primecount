f642d975ce5808c7fb585baf70c693432efdb70f
kimwalisch <kim.walisch@gmail.com>
2019-10-24 13:41:30 +0200

    Add backup

--- src/gourdon/B.cpp
+++ src/gourdon/B.cpp
@@ -21,10 +21,12 @@
 #include <gourdon.hpp>
 #include <primecount-internal.hpp>
 #include <primesieve.hpp>
+#include <calculator.hpp>
 #include <aligned_vector.hpp>
 #include <int128_t.hpp>
 #include <min.hpp>
 #include <imath.hpp>
+#include <json.hpp>
 #include <print.hpp>
 
 #include <stdint.h>
@@ -36,6 +38,111 @@ using namespace std;
 using namespace primecount;
 
 namespace {
+
+/// backup to file every 60 seconds
+bool is_backup(double time)
+{
+  double seconds = get_time() - time;
+  return seconds > 60;
+}
+
+/// backup intermediate result
+template <typename T, typename J>
+void backup(J& json,
+            T x,
+            int64_t y,
+            int64_t z,
+            int64_t low,
+            int64_t thread_distance,
+            T pix_total,
+            T b,
+            double time)
+{
+  double percent = get_percent(low, z);
+
+  json["B"]["x"] = to_string(x);
+  json["B"]["y"] = y;
+  json["B"]["low"] = low;
+  json["B"]["thread_distance"] = thread_distance;
+  json["B"]["pix_total"] = to_string(pix_total);
+  json["B"]["b"] = to_string(b);
+  json["B"]["percent"] = percent;
+  json["B"]["seconds"] = get_time() - time;
+
+  store_backup(json);
+}
+
+/// backup result
+template <typename T>
+void backup(T x,
+            int64_t y,
+            T b,
+            double time)
+{
+  auto json = load_backup();
+
+  if (json.find("B") != json.end())
+    json.erase("B");
+
+  json["B"]["x"] = to_string(x);
+  json["B"]["y"] = y;
+  json["B"]["b"] = to_string(b);
+  json["B"]["percent"] = 100.0;
+  json["B"]["seconds"] = get_time() - time;
+
+  store_backup(json);
+}
+
+/// resume computation
+template <typename T, typename J>
+bool resume(J& json,
+            T x,
+            int64_t y,
+            int64_t& low,
+            int64_t& thread_distance,
+            T& pix_total,
+            T& b,
+            double& time)
+{
+  if (is_resume(json, "B", x, y))
+  {
+    double seconds = json["B"]["seconds"];
+    low = json["B"]["low"];
+    thread_distance = json["B"]["thread_distance"];
+    pix_total = calculator::eval<T>(json["B"]["pix_total"]);
+    b = calculator::eval<T>(json["B"]["b"]);
+    time = get_time() - seconds;
+    return true;
+  }
+
+  return false;
+}
+
+/// resume result
+template <typename T>
+bool resume(T x,
+            int64_t y,
+            T& b,
+            double& time)
+{
+  auto json = load_backup();
+
+  if (is_resume(json, "B", x, y))
+  {
+    double percent = json["B"]["percent"];
+    double seconds = json["B"]["seconds"];
+    print_resume(percent, x);
+
+    if (!json["B"].count("low"))
+    {
+      b = calculator::eval<T>(json["B"]["b"]);
+      time = get_time() - seconds;
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /// Count the primes inside [prime, stop]
 int64_t count_primes(primesieve::iterator& it, int64_t& prime, int64_t stop)
@@ -117,7 +224,7 @@ T B_thread(T x,
 /// Memory usage: O(z^(1/2))
 ///
 template <typename T>
-T B_OpenMP(T x, int64_t y, int threads)
+T B_OpenMP(T x, int64_t y, int threads, double& time)
 {
   if (x < 4)
     return 0;
@@ -132,25 +239,38 @@ T B_OpenMP(T x, int64_t y, int threads)
 
   aligned_vector<int64_t> pix(threads);
   aligned_vector<int64_t> pix_counts(threads);
-  
+
+  auto json = load_backup();
+  if (!resume(json, x, y, low, thread_distance, pix_total, sum, time))
+    if (json.find("B") != json.end())
+      json.erase("B");
+
+  double last_backup_time = get_time();
+
   while (low < z)
   {
     int64_t max_threads = ceil_div(z - low, thread_distance);
     threads = in_between(1, threads, max_threads);
-    double time = get_time();
+    double t = get_time();
 
     #pragma omp parallel for num_threads(threads) reduction(+: sum)
     for (int i = 0; i < threads; i++)
       sum += B_thread(x, y, z, low, i, thread_distance, pix[i], pix_counts[i]);
 
     low += thread_distance * threads;
-    balanceLoad(&thread_distance, low, z, threads, time);
+    balanceLoad(&thread_distance, low, z, threads, t);
 
     // add missing sum contributions in order
     for (int i = 0; i < threads; i++)
     {
       sum += pix_total * pix_counts[i];
       pix_total += pix[i];
+    }
+
+    if (is_backup(last_backup_time))
+    {
+      backup(json, x, y, z, low, thread_distance, pix_total, sum, time);
+      last_backup_time = get_time();
     }
 
     if (is_print())
@@ -175,7 +295,13 @@ int64_t B(int64_t x, int64_t y, int threads)
   print_gourdon(x, y, threads);
 
   double time = get_time();
-  int64_t b = B_OpenMP((intfast64_t) x, y, threads);
+  int64_t b = 0;
+
+  if (!resume(x, y, b, time))
+  {
+    b = B_OpenMP((intfast64_t) x, y, threads, time);
+    backup(x, y, b, time);
+  }
 
   print("B", b, time);
   return b;
@@ -190,7 +316,13 @@ int128_t B(int128_t x, int64_t y, int threads)
   print_gourdon(x, y, threads);
 
   double time = get_time();
-  int128_t b = B_OpenMP((intfast128_t) x, y, threads);
+  int128_t b = 0;
+
+  if (!resume(x, y, b, time))
+  {
+    b = B_OpenMP((intfast128_t) x, y, threads, time);
+    backup(x, y, b, time);
+  }
 
   print("B", b, time);
   return b;
