0e93c4d7b976c1d46173afdcd44bcbdf73a6f5d0
kimwalisch <kim.walisch@gmail.com>
2020-08-14 08:48:51 +0200

    Update comment

--- src/gourdon/AC_libdivide.cpp
+++ src/gourdon/AC_libdivide.cpp
@@ -367,13 +367,13 @@ T AC_OpenMP(T x,
     T xlow = x / low;
     T xhigh = x / high;
 
-    // Lower bounds of the C2 formula
+    // Lower bounds of C2 formula
     min_b = max3(k, pi_sqrtz, pi_root3_xy);
     min_b = max(min_b, pi[isqrt(low)]);
     min_b = max(min_b, pi[min(xhigh / y, x13)]);
     min_b += 1;
 
-    // Upper bound of the A & C2 formulas:
+    // Upper bound of A & C2 formulas:
     // x / (p * m) >= low
     // p * next_prime(p) <= x / low
     // p <= sqrt(x / low)
