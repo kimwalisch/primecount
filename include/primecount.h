c7b94c58f163134553e632434900ebd4ef30f370
kimwalisch <kim.walisch@gmail.com>
2020-09-29 10:16:17 +0200

    Increase version number

--- include/primecount.h
+++ include/primecount.h
@@ -16,9 +16,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define PRIMECOUNT_VERSION "6.1"
+#define PRIMECOUNT_VERSION "6.2"
 #define PRIMECOUNT_VERSION_MAJOR 6
-#define PRIMECOUNT_VERSION_MINOR 1
+#define PRIMECOUNT_VERSION_MINOR 2
 
 #ifdef __cplusplus
 extern "C" {
