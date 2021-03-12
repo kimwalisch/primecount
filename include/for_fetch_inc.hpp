064d055ec4f65b3f9be567ffbcfcb96a1b112723
kimwalisch <kim.walisch@gmail.com>
2021-03-12 19:50:37 +0100

    Update documentation

--- include/for_fetch_inc.hpp
+++ include/for_fetch_inc.hpp
@@ -10,17 +10,27 @@
 #ifndef FOR_FETCH_INC
 #define FOR_FETCH_INC
 
-/// for (b = start; condition; b = atomic_i++)
-/// This is a for loop with with dynamic thread scheduling for use
-/// inside of a parallel region. We use this instead of OpenMP's
-/// dynamic thread scheduling because the Clang compiler currently
-/// (2021) has a severe scaling issue on PCs/servers with a large
-/// number of CPU cores. GCC has no such scaling issue.
+#include <atomic>
+
+/// for_fetch_inc() is a for loop with dynamic thread scheduling for
+/// use inside of an OpenMP parallel region. We use this instead of
+/// OpenMP's dynamic thread scheduling because the Clang compiler
+/// currently (Clang 11, 2021) has a severe scaling issue on PCs &
+/// servers with a large number of CPU cores. The scaling issue
+/// occurred when computing AC(x) with x >= 1e22. GCC has no such
+/// scaling issue but for_fetch_inc() runs slightly faster on GCC as
+/// well.
+///
+/// for_fetch_inc(atomic_i, start, condition)
+/// Is the same as:
+///
+/// #pragma omp for schedule(dynamic)
+/// for (auto b = start; condition; b++)
 ///
 #define for_fetch_inc(atomic_i, start, condition) \
   _Pragma("omp single") \
   atomic_i = start; \
-  for (auto b = atomic_i.fetch_add(1, memory_order_relaxed); \
-       condition; b = atomic_i.fetch_add(1, memory_order_relaxed))
+  for (auto b = atomic_i.fetch_add(1, std::memory_order_relaxed); \
+       condition; b = atomic_i.fetch_add(1, std::memory_order_relaxed))
 
 #endif
