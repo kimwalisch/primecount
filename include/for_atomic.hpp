771f0030f84ae25a9d33fd9ee8c6fa619ff87dda
Kim Walisch <kim.walisch@gmail.com>
2021-03-15 19:38:26 +0100

    Update documentation

--- include/for_atomic.hpp
+++ include/for_atomic.hpp
@@ -1,11 +1,18 @@
 ///
 /// @file  for_atomic.hpp
 ///        The for_atomic_inc() macro is a workaround for a severe
-///        scaling issue in Clang's OpenMP library when using a
-///        parallel for loop with schedule(dynamic).
+///        scaling issue in Clang's OpenMP library when using a parallel
+///        for loop with schedule(dynamic). More details can be found
+///        in my bug report: https://bugs.llvm.org/show_bug.cgi?id=49588
 ///
-///        More details can be found in my bug report:
-///        https://bugs.llvm.org/show_bug.cgi?id=49588
+///        Note that the atomic for loops in this file are optimal for
+///        primecount's use case. Unlike GCC's OpenMP library which
+///        uses atomics with memory_order_acq_rel our implementation
+///        uses atomics with memory_order_relaxed because our threads
+///        are completely independent from each other. This improves
+///        performance on CPU architectures with a weak memory model
+///        such as ARM64. Also since our algorithms perform best using
+///        a chunk size of 1 we can hardcode that value.
 ///
 /// Copyright (C) 2021 Kim Walisch, <kim.walisch@gmail.com>
 ///
