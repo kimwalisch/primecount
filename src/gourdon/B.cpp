27459a5e0683fb3374b1a64b73a012a9a676d5ee
kimwalisch <kim.walisch@gmail.com>
2021-03-19 12:51:48 +0100

    Refactor

--- src/gourdon/B.cpp
+++ src/gourdon/B.cpp
@@ -78,9 +78,8 @@ B_thread(T x,
     // thread sieves [low, z[
     z = min(low + thread_dist, z);
     int64_t sqrtx = isqrt(x);
-    int64_t xz = min(x / z, sqrtx);
+    int64_t start = max(y, min(x / z, sqrtx));
     int64_t stop = min(x / low, sqrtx);
-    int64_t start = max(xz, y);
 
     primesieve::iterator it(low - 1, z);
     primesieve::iterator rit(stop + 1, start);
