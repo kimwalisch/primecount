170745ba54aff3a840515843b9523f422feeca02
kimwalisch <kim.walisch@gmail.com>
2021-04-04 10:26:11 +0200

    Simplify for_atomic_add()

--- include/for_atomic.hpp
+++ include/for_atomic.hpp
@@ -1,6 +1,6 @@
 ///
 /// @file  for_atomic.hpp
-///        The for_atomic_inc() macro is a workaround for a severe
+///        The for_atomic_add() macro is a workaround for a severe
 ///        scaling issue in Clang's OpenMP library when using a parallel
 ///        for loop with schedule(dynamic). More details can be found
 ///        in my bug report: https://bugs.llvm.org/show_bug.cgi?id=49588
@@ -25,36 +25,6 @@
 
 #include <atomic>
 
-/// for_atomic_inc() is a for loop with dynamic thread scheduling for use
-/// inside of an OpenMP parallel region. We use this instead of OpenMP's
-/// dynamic thread scheduling because the Clang compiler currently
-/// (Clang 11, 2021) has a severe scaling issue on PCs & servers with a
-/// large number of CPU cores. The scaling issue occurred when computing
-/// AC(x) with x >= 1e22.
-///
-/// for_atomic_inc(start, condition, atomic_b)
-/// Is the same as:
-///
-/// #pragma omp for nowait schedule(dynamic)
-/// for (auto b = start; condition; b++)
-///
-#define for_atomic_inc(start, condition, atomic_b) \
-  /* for_atomic_inc() is used in the computation of the AC formula */ \
-  /* where the individual threads are completely independent of each */ \
-  /* other, hence there is no communication between the threads and the */ \
-  /* threads only read from memory but do not write to memory. */ \
-  /* Because of these constraints we can use relaxed atomics, i.e. our */ \
-  /* program will work correctly even if the CPU reorders memory reads */ \
-  /* and writes before or after the atomic_b variable we use as loop */ \
-  /* counter. Note that the fetch_add() instruction can never be */ \
-  /* reordered before compare_exchange_strong() by the CPU because this */ \
-  /* would change the program behavior in single-thread mode. */ \
-  for (decltype(start) is_first_thread = -1, \
-       b = (atomic_b.compare_exchange_strong(is_first_thread, start, std::memory_order_relaxed)) \
-       ? atomic_b.fetch_add(1, std::memory_order_relaxed) \
-       : atomic_b.fetch_add(1, std::memory_order_relaxed); \
-       condition; b = atomic_b.fetch_add(1, std::memory_order_relaxed))
-
 /// for_atomic_add() is a for loop with dynamic thread scheduling for use
 /// inside of an OpenMP parallel region. We use this instead of OpenMP's
 /// dynamic thread scheduling because the Clang compiler currently
@@ -62,28 +32,15 @@
 /// large number of CPU cores. The scaling issue occurred when computing
 /// AC(x) with x >= 1e22.
 ///
-/// for_atomic_add(start, condition, inc, atomic_b)
+/// for_atomic_add(var, condition, inc)
 /// Is the same as:
 ///
 /// #pragma omp for nowait schedule(dynamic)
-/// for (auto b = start; condition; b += inc)
+/// for (auto var = atomic_var; condition; var += inc)
 ///
-#define for_atomic_add(start, condition, inc, atomic_b) \
-  /* for_atomic_add() is used in the computation of the AC formula */ \
-  /* where the individual threads are completely independent of each */ \
-  /* other, hence there is no communication between the threads and the */ \
-  /* threads only read from memory but do not write to memory. */ \
-  /* Because of these constraints we can use relaxed atomics, i.e. our */ \
-  /* program will work correctly even if the CPU reorders memory reads */ \
-  /* and writes before or after the atomic_b variable we use as loop */ \
-  /* counter. Note that the fetch_add() instruction can never be */ \
-  /* reordered before compare_exchange_strong() by the CPU because this */ \
-  /* would change the program behavior in single-thread mode. */ \
-  for (decltype(start) is_first_thread = -1, \
-       b = (atomic_b.compare_exchange_strong(is_first_thread, start, std::memory_order_relaxed)) \
-       ? atomic_b.fetch_add(inc, std::memory_order_relaxed) \
-       : atomic_b.fetch_add(inc, std::memory_order_relaxed); \
-       condition; b = atomic_b.fetch_add(inc, std::memory_order_relaxed))
+#define for_atomic_add(var, condition, inc) \
+  for (auto var = atomic_##var.fetch_add(inc, std::memory_order_relaxed); \
+       condition; var = atomic_##var.fetch_add(inc, std::memory_order_relaxed))
 
 /// parallel_for_atomic_inc() is a parallel for loop with dynamic thread
 /// scheduling. We use this instead of OpenMP's dynamic thread scheduling
