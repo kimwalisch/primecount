c2465d7c57809df76b177e6bdcbee7111ecdc541
kimwalisch <kim.walisch@gmail.com>
2021-08-12 17:53:52 +0200

    Cache small primes <= 1009

--- src/nth_prime.cpp
+++ src/nth_prime.cpp
@@ -1,8 +1,8 @@
 ///
 /// @file  nth_prime.cpp
-/// @brief Find the nth prime
+/// @brief Find the nth prime.
 ///
-/// Copyright (C) 2019 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2021 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
@@ -16,37 +16,58 @@
 #include <string>
 #include <array>
 
-using namespace std;
-
 namespace {
 
 // Number of primes < 2^63
 const int64_t max_n = 216289611853439384ll;
 
 // primes[1] = 2, primes[2] = 3, ...
-const array<int, 10> primes = { 0, 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+const std::array<int16_t, 170> primes =
+{
+    0,   2,   3,   5,   7,  11,  13,  17,  19,  23, 
+   29,  31,  37,  41,  43,  47,  53,  59,  61,  67, 
+   71,  73,  79,  83,  89,  97, 101, 103, 107, 109, 
+  113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 
+  173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 
+  229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 
+  281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 
+  349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 
+  409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 
+  463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 
+  541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 
+  601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 
+  659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 
+  733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 
+  809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 
+  863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 
+  941, 947, 953, 967, 971, 977, 983, 991, 997, 1009
+};
 
-}
+} // namespace
 
 namespace primecount {
 
-/// Find the nth prime using a combination of the prime counting
-/// function and the segmented sieve of Eratosthenes.
+/// Find the nth prime using the prime counting function
+/// and the segmented sieve of Eratosthenes.
 /// Run time: O(x^(2/3) / (log x)^2)
 /// Memory usage: O(x^(1/2))
 ///
 int64_t nth_prime(int64_t n, int threads)
 {
-  if (n < 2)
+  // When I first implemented nth_prime(n) in primesieve I used
+  // nth_prime(0) = 2 because Mathematica's Prime[n] function
+  // also returned Prime[0] = 2. However nowadays Mathematica
+  // and most other libraries return an error if the user enters
+  // Prime[0]. In order not to break backwards compatibility I
+  // keep nth_prime(0) = 2 in both primesieve and primecount.
+  if (n < 1)
     return primes[1];
-  if (n < 10)
+
+  if (n < (int64_t) primes.size())
     return primes[n];
 
   if (n > max_n)
-  {
-    string msg("nth_prime(n): n must be <= " + to_string(max_n));
-    throw primecount_error(msg);
-  }
+    throw primecount_error("nth_prime(n): n must be <= " + std::to_string(max_n));
 
   int64_t prime = 0;
 
