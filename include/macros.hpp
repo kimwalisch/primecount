cbcb76fc9e30a1e354a7aca2a7969c10656acdf3
kimwalisch <kim.walisch@gmail.com>
2021-09-24 11:11:57 +0200

    Fix indentation

--- include/macros.hpp
+++ include/macros.hpp
@@ -66,10 +66,10 @@
 // warning: 'unused' attribute ignored [-Wattributes]
 #if defined(__GNUC__) && \
    !defined(__clang__)
-   #if __GNUC__ < 12
-     #undef MAYBE_UNUSED
-     #define MAYBE_UNUSED
-   #endif
+  #if __GNUC__ < 12
+    #undef MAYBE_UNUSED
+    #define MAYBE_UNUSED
+  #endif
 #endif
 
 #if defined(__GNUC__) || \
