998c3827139efab25a68874130078de8612ea591
kimwalisch <kim.walisch@gmail.com>
2022-01-02 10:06:25 +0100

    Add more tests

--- scripts/benchmark-vs-prev-release.sh
+++ scripts/benchmark-vs-prev-release.sh
@@ -25,7 +25,8 @@ then
     exit 1
 fi
 
-rm -rf build-curr-release build-prev-release 
+rm -rf build-curr-release build-prev-release
+current_git_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Build the latest code
 cmake -S . -B build-curr-release  -G "Unix Makefiles"
@@ -36,6 +37,9 @@ git checkout $(git describe --tags --abbrev=0)
 cmake -S . -B build-prev-release  -G "Unix Makefiles"
 cmake --build build-prev-release -- -j4
 
+# Go back to initial branch
+git checkout $current_git_branch
+
 echo "=== Old code (previous release) ==="
 build-prev-release/./primecount --version
 echo ""
@@ -44,50 +48,94 @@ echo "=== New code ==="
 build-curr-release/./primecount --version
 echo ""
 
-# New code must not be more than 
-# 2% slower than old code.
-factor=1.02
+function benchmark_test_option {
+    # New code must not be more than 
+    # 5% slower than old code.
+    factor=1.05
 
-# Test failure must be observed 3 times,
-# we try to avoid false negatives.
-for j in {1..3}
-do
-    echo "=== Benchmark $j ===="
-    echo ""
-
-    total_seconds1=0
-    total_seconds2=0
-
-    for i in {1..10}
+    # Test failure must be observed 3 times,
+    # we try to avoid false negatives.
+    for j in {1..3}
     do
-        seconds1=$(build-prev-release/./primecount 1e17 --time | grep Seconds | cut -d' ' -f2)
+        echo "=== Benchmark primecount --test ===="
+        echo ""
+
+        seconds1=$(time --format=%e build-prev-release/./primecount -t4 --test 2>&1 | tail -n1)
         echo "Seconds old code: $seconds1"
-        total_seconds1=$(echo "scale=3; $total_seconds1 + $seconds1" | bc -l)
         sleep 1
-        seconds2=$(build-curr-release/./primecount 1e17 --time | grep Seconds | cut -d' ' -f2)
+        seconds2=$(time --format=%e build-curr-release/./primecount -t4 --test 2>&1 | tail -n1)
         echo "Seconds new code: $seconds2"
         echo ""
-        total_seconds2=$(echo "scale=3; $total_seconds2 + $seconds2" | bc -l)
         sleep 1
+
+        limit=$(echo "scale=3; $seconds1 * $factor" | bc -l)
+        new_code_is_fast=$(echo $seconds2'<='$limit | bc -l)
+        new_code_percent=$(echo "scale=1; 100 * $seconds2 / $seconds1" | bc -l)
+
+        echo "Old code: 100.0%"
+        echo "New code: $new_code_percent%"
+
+        if [ $new_code_is_fast -eq 1 ]
+        then
+            echo "primecount --test performance test passed successfully!"
+            return
+        fi
     done
 
-    echo "Total seconds old code: $total_seconds1"
-    echo "Total seconds new code: $total_seconds2"
-    echo ""
+    echo "primecount --test is more than $factor times slower than previous release!"
+    exit 1
+}
 
-    limit=$(echo "scale=3; $total_seconds1 * $factor" | bc -l)
-    new_code_is_fast=$(echo $total_seconds2'<='$limit | bc -l)
-    new_code_percent=$(echo "scale=1; 100 * $total_seconds2 / $total_seconds1" | bc -l)
+function benchmark_pi_1e17 {
+    # New code must not be more than 
+    # 2% slower than old code.
+    factor=1.02
 
-    echo "Old code: 100.0%"
-    echo "New code: $new_code_percent%"
+    # Test failure must be observed 3 times,
+    # we try to avoid false negatives.
+    for j in {1..3}
+    do
+        echo "=== Benchmark PrimePi(1e17) ===="
+        echo ""
 
-    if [ $new_code_is_fast -eq 1 ]
-    then
-        echo "New code successfully passed performance test!"
-        exit 0
-    fi
-done
+        total_seconds1=0
+        total_seconds2=0
 
-echo "New code is more than $factor times slower than previous release!"
-exit 1
+        for i in {1..10}
+        do
+            seconds1=$(build-prev-release/./primecount 1e17 -t4 --time | grep Seconds | cut -d' ' -f2)
+            echo "Seconds old code: $seconds1"
+            total_seconds1=$(echo "scale=3; $total_seconds1 + $seconds1" | bc -l)
+            sleep 1
+            seconds2=$(build-curr-release/./primecount 1e17 -t4 --time | grep Seconds | cut -d' ' -f2)
+            echo "Seconds new code: $seconds2"
+            echo ""
+            total_seconds2=$(echo "scale=3; $total_seconds2 + $seconds2" | bc -l)
+            sleep 1
+        done
+
+        echo "Total seconds old code: $total_seconds1"
+        echo "Total seconds new code: $total_seconds2"
+        echo ""
+
+        limit=$(echo "scale=3; $total_seconds1 * $factor" | bc -l)
+        new_code_is_fast=$(echo $total_seconds2'<='$limit | bc -l)
+        new_code_percent=$(echo "scale=1; 100 * $total_seconds2 / $total_seconds1" | bc -l)
+
+        echo "Old code: 100.0%"
+        echo "New code: $new_code_percent%"
+
+        if [ $new_code_is_fast -eq 1 ]
+        then
+            echo "PrimePi(1e17) performance test passed successfully!"
+            return
+        fi
+    done
+
+    echo "PrimePi(1e17) is more than $factor times slower than previous release!"
+    exit 1
+}
+
+# Execute benchmark functions
+benchmark_test_option
+benchmark_pi_1e17
