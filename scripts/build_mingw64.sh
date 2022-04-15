a3ff64182413bc2fd00da7f7e9f931c1256a3fc4
kimwalisch <kim.walisch@gmail.com>
2022-04-15 16:08:13 +0200

    Remove legacy code

--- scripts/build_mingw64.sh
+++ scripts/build_mingw64.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Usage: scripts/build_mingw64.sh
-# Builds primecount and primecount-backup release binaries that
-# are statically linked and ready for distribution.
+# Builds a primecount release binary that is statically linked
+# and ready for distribution.
 
 # Exit if any error occurs
 set -e
@@ -34,10 +34,9 @@ git diff --exit-code > /dev/null || handle_error "repo must not have any uncommi
 
 # Build primecount binary ##########################################
 
-git checkout master
 git pull
-mkdir build-master
-cd build-master
+mkdir build-release
+cd build-release
 cmake .. -G "Unix Makefiles" -DCMAKE_CXX_FLAGS="-flto -static -static-libgcc -static-libstdc++ -Wall -Wextra -pedantic -D_WIN32_WINNT=0x601" -DWITH_FLOAT128=ON
 make -j8
 rm primecount.exe
@@ -79,68 +78,15 @@ sed -i "3 s/.*/Copyright \(c\) 2013 - $YEAR, Kim Walisch\./" COPYING
 
 zip primecount-$VERSION-win-x64.zip primecount.exe README.txt COPYING
 cp primecount-$VERSION-win-x64.zip ..
+
 ./primecount --test
 echo ""
-cd ..
-
-# Build primecount-backup binary ###################################
-
-cd ..
-git checkout backup3
-git pull
-mkdir build-backup3
-cd build-backup3
-cmake .. -G "Unix Makefiles" -DCMAKE_CXX_FLAGS="-flto -static -static-libgcc -static-libstdc++ -Wall -Wextra -pedantic -D_WIN32_WINNT=0x601" -DWITH_FLOAT128=ON
-make -j8
-rm primecount.exe
-
-# Remove unnecessary libraries for linking,
-# keep only GCC libraries + kernel32.
-sed -i 's/-lkernel32.*/-lkernel32/g' CMakeFiles/primecount.dir/linklibs.rsp
-sed -i 's/libgomp\.dll\.a/libgomp\.a/g' CMakeFiles/primecount.dir/linklibs.rsp
-
-# Verify that sed has worked correctly,
-# last word should be -lkernel32.
-[ "$(grep -o '[^ ]\+$' CMakeFiles/primecount.dir/linklibs.rsp)" = "-lkernel32" ] || handle_error "failed updating linklibs.rsp"
-
-make
-strip primecount.exe
-
-# Create a release zip archive
-wget https://github.com/kimwalisch/primecount/releases/download/v6.0-backup/primecount-backup-6.0-win64.zip
-unzip primecount-backup-6.0-win64.zip -d primecount-backup-$VERSION-win-x64
-rm primecount-backup-6.0-win64.zip
-
 echo ""
-echo ""
-echo "Old file size: $(ls -l --block-size=K primecount-backup-$VERSION-win-x64/primecount.exe)"
-echo "New file size: $(ls -l --block-size=K primecount.exe)"
-echo ""
-echo ""
+./primecount 1e18 -s
 
-mv -f primecount.exe primecount-backup-$VERSION-win-x64
-cd primecount-backup-$VERSION-win-x64
-sed -i "1 s/.*/primecount-backup $VERSION/" README.txt
-sed -i "2 s/.*/$FULL_DATE/" README.txt
-sed -i "3 s/.*/Copyright \(c\) 2013 - $YEAR, Kim Walisch\./" COPYING
-
-# Verify sed has worked correctly
-[ "$(sed -n '1p' < README.txt)" = "primecount-backup $VERSION" ] || handle_error "failed updating README.txt"
-[ "$(sed -n '2p' < README.txt)" = "$FULL_DATE" ] || handle_error "failed updating README.txt"
-[ "$(sed -n '3p' < COPYING)" = "Copyright (c) 2013 - $YEAR, Kim Walisch." ] || handle_error "failed updating COPYING"
-
-zip primecount-backup-$VERSION-win-x64.zip primecount.exe README.txt COPYING worktodo.sh worktodo.txt
-cp primecount-backup-$VERSION-win-x64.zip ..
-
-echo ""
-echo "Testing:"
-./primecount 1e17 --time
-./primecount 1e17
 cd ..
 
 ####################################################################
 
-git checkout master
-
 echo ""
-echo "Release binaries built successfully!"
+echo "Release binary built successfully!"
