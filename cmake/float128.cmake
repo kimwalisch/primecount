e6cd740a6a5936d97c71d655279b1bfae8a0f96f
kimwalisch <kim.walisch@gmail.com>
2022-12-29 10:57:42 +0100

    Add more comments

--- cmake/float128.cmake
+++ cmake/float128.cmake
@@ -1,6 +1,15 @@
 # Check if the compiler and CPU architecture support
 # 128-bit floating point numbers & libquadmath.
+
 # As of 2022 only GCC supports libquadmath.
+
+# Note that GCC currently warns when compiling using -pedantic
+# because of the use of the non-standard Q suffix:
+# "warning: non-standard suffix on floating constant [-Wpedantic]".
+# And there is currently (2022) no way to suppress this warning,
+# see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=92826.
+# Because of this issue this test will fail when compiling with
+# GCC and -pedantic -Werror (which is used e.g. in our CI tests).
 
 include(CheckCXXSourceCompiles)
 include(CMakePushCheckState)
@@ -12,7 +21,7 @@ set(CMAKE_REQUIRED_LIBRARIES "quadmath")
 check_cxx_source_compiles("
     #include <quadmath.h>
     int main() {
-        __float128 t = 1e10;
+        __float128 t = 10000000000.0Q;
         __float128 old_term = FLT128_MAX;
 
         for (int i = 0; i < 10; i++)
