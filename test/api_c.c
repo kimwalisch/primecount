0b3af4049909d861acb586ce2c99a942c0f9a2c9
Kim Walisch <kim.walisch@gmail.com>
2023-03-26 14:36:31 +0200

    Convert api_c.cpp to api_c.c

--- test/api_c.c
+++ test/api_c.c
@@ -1,5 +1,5 @@
 ///
-/// @file   api_c.cpp
+/// @file   api_c.c
 /// @brief  Test primecount's C API.
 ///
 /// Copyright (C) 2023 Kim Walisch, <kim.walisch@gmail.com>
@@ -11,72 +11,77 @@
 #include <primecount.h>
 
 #include <stdint.h>
-#include <iostream>
-#include <string>
-#include <cstdlib>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void check(bool OK)
+void check(int OK)
 {
-  std::cout << "   " << (OK ? "OK" : "ERROR") << "\n";
-  if (!OK)
-    std::exit(1);
+  if (OK)
+    printf("   OK\n");
+  else
+  {
+    printf("   ERROR\n");
+    exit(1);
+  }
 }
 
 int main()
 {
-  std::cout << "primecount version: " << primecount_version() << std::endl;
-  std::cout << "threads: " << primecount_get_num_threads() << std::endl;
+  printf("primecount version: %s\n", primecount_version());
+  printf("threads: %d\n", primecount_get_num_threads());
   
   primecount_set_num_threads(3);
-  std::cout << "new threads: " << primecount_get_num_threads() << std::endl;
+  printf("new threads: %d\n", primecount_get_num_threads());
 
   // Test 64-bit pi(-1)
   int64_t n = -1;
   int64_t res = primecount_pi(n);
-  std::cout << "primecount_pi(" << n << ") = " << res;
+  printf("primecount_pi(%"PRId64") = %"PRId64, n, res);
   check(res == 0);
 
   // Test 128-bit pi(-1)
   char out[32];
   primecount_pi_str("-1", out, sizeof(out));
-  std::cout << "primecount_pi_str(-1) = " << out;
-  check(std::string(out) == "0");
+  printf("primecount_pi_str(-1) = %s", out);
+  check(strcmp(out, "0") == 0);
 
   n = (int64_t) 1e10;
   res = primecount_pi(n);
-  std::cout << "primecount_pi(" << n << ") = " << res;
+  printf("primecount_pi(%"PRId64") = %"PRId64, n, res);
   check(res == 455052511);
 
   n = 455052511;
   res = primecount_nth_prime(n);
-  std::cout << "primecount_nth_prime(" << n << ") = " << res;
+  printf("primecount_nth_prime(%"PRId64") = %"PRId64, n, res);
   check(res == 9999999967);
 
   // nth_prime(-1) is an error and should hence return -1
   // which indicates an error in the libprimecount C API.
   n = -1;
   res = primecount_nth_prime(n);
-  std::cout << "primecount_nth_prime(" << n << ") = " << res;
+  printf("primecount_nth_prime(%"PRId64") = %"PRId64, n, res);
   check(res == -1);
 
   n = (int64_t) 1e12;
   int64_t a = 78498;
   res = primecount_phi(n, a);
-  std::cout << "primecount_phi(" << n << ", " << a << ") = " << res;
+  printf("primecount_phi(%"PRId64", %"PRId64") = %"PRId64, n , a, res);
   check(res == 37607833521);
 
   n = -1;
   res = primecount_phi(n, a);
-  std::cout << "primecount_phi(" << n << ", " << a << ") = " << res;
+  printf("primecount_phi(%"PRId64", %"PRId64") = %"PRId64, n , a, res);
   check(res == 0);
 
   const char* in = "1000000000000";
   primecount_pi_str(in, out, sizeof(out));
-  std::cout << "primecount_pi_str(" << in << ") = " << out;
-  check(std::string(out) == "37607912018");
+  printf("primecount_pi_str(%s) = %s", in, out);
+  check(strcmp(out, "37607912018") == 0);
 
-  std::cout << std::endl;
-  std::cout << "All tests passed successfully!" << std::endl;
+  printf("\n");
+  printf("All tests passed successfully!\n");
 
   return 0;
 }
