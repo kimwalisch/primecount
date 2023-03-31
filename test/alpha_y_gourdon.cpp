7c46cf23aaac0249e92df8fbb5e1f7753193bb53
Kim Walisch <kim.walisch@gmail.com>
2023-03-31 22:44:26 +0200

    Test 128-bit pi(x)

--- test/alpha_y_gourdon.cpp
+++ test/alpha_y_gourdon.cpp
@@ -6,7 +6,7 @@
 ///         factors we can make sure that all array sizes
 ///         (and other bounds) are accurate.
 ///
-/// Copyright (C) 2019 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2023 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
@@ -33,13 +33,13 @@ void check(bool OK)
 
 int main()
 {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-
   int64_t min = (int64_t) 1e9;
   int64_t max = min * 2;
-  std::uniform_int_distribution<int64_t> dist(min, max);
   int threads = get_num_threads();
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<int64_t> dist(min, max);
 
   for (int i = 0; i < 20; i++)
   {
@@ -49,12 +49,31 @@ int main()
     for (double alpha_y = 1; alpha_y <= iroot<6>(x); alpha_y++)
     {
       set_alpha_y(alpha_y);
-      int64_t res2 = pi_gourdon(x, threads);
+      int64_t res2 = pi_gourdon_64(x, threads);
 
-      std::cout << "pi_gourdon(" << x << ") = " << res2;
+      std::cout << "pi_gourdon_64(" << x << ") = " << res2;
       check(res1 == res2);
     }
   }
+
+#ifdef HAVE_INT128_T
+
+  for (int i = 0; i < 20; i++)
+  {
+    int64_t x = dist(gen);
+    int64_t res1 = pi_meissel(x, threads);
+
+    for (double alpha_y = 1; alpha_y <= iroot<6>(x); alpha_y++)
+    {
+      set_alpha_y(alpha_y);
+      int64_t res2 = pi_gourdon_128(x, threads);
+
+      std::cout << "pi_gourdon_128(" << x << ") = " << res2;
+      check(res1 == res2);
+    }
+  }
+
+#endif
 
   std::cout << std::endl;
   std::cout << "All tests passed successfully!" << std::endl;
