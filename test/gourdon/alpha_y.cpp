a8c464946a0da8808c01139af850e62928e8395b
Kim Walisch <kim.walisch@gmail.com>
2023-04-02 11:11:37 +0200

    Increase number of tests

--- test/gourdon/alpha_y.cpp
+++ test/gourdon/alpha_y.cpp
@@ -76,14 +76,14 @@ int main()
 
   // Test medium values of x
   {
-    int64_t min = (int64_t) 1e8;
-    int64_t max = min * 2;
+    int64_t min = (int64_t) 1e3;
+    int64_t max = (int64_t) 1e8;
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int64_t> dist(min, max);
 
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 100; i++)
     {
       int64_t x = dist(gen);
       int64_t res1 = pi_meissel(x, threads);
@@ -99,7 +99,7 @@ int main()
     }
 
   #ifdef HAVE_INT128_T
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 100; i++)
     {
       int64_t x = dist(gen);
       int64_t res1 = pi_meissel(x, threads);
--- test/lmo/alpha_lmo.cpp
+++ test/lmo/alpha_lmo.cpp
@@ -58,14 +58,14 @@ int main()
 
   // Test medium values of x
   {
-    int64_t min = (int64_t) 1e7;
-    int64_t max = min * 2;
+    int64_t min = (int64_t) 1e3;
+    int64_t max = (int64_t) 2e7;
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int64_t> dist(min, max);
 
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 100; i++)
     {
       int64_t x = dist(gen);
       int64_t res1 = pi_meissel(x, threads);
