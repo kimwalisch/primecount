79396e5bf4390784c71e3c0e32dad7e38b195e0f
Kim Walisch <kim.walisch@gmail.com>
2023-04-02 10:15:39 +0200

    Add more tests

--- test/P3.cpp
+++ test/P3.cpp
@@ -4,7 +4,7 @@
 ///         that counts the numbers <= x that have exactly
 ///         3 prime factors each exceeding the a-th prime.
 ///
-/// Copyright (C) 2022 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2023 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
@@ -32,34 +32,76 @@ void check(bool OK)
 
 int main()
 {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<int> dist(40000, 50000);
-
-  int threads = 1;
-  int64_t x = dist(gen);
-  auto primes = generate_primes<int64_t>(x);
-
-  for (int64_t a = 1; primes[a] <= iroot<3>(x); a++)
+  // Test small values of x
   {
-    int64_t p3 = 0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(2, 1000);
 
-    for (size_t b = a + 1; b < primes.size(); b++)
+    for (int i = 0; i < 100; i++)
     {
-      for (size_t c = b; c < primes.size(); c++)
+      int threads = 1;
+      int64_t x = dist(gen);
+      auto primes = generate_primes<int64_t>(x);
+
+      for (int64_t a = 1; primes[a] <= iroot<3>(x); a++)
       {
-        for (size_t d = c; d < primes.size(); d++)
+        int64_t p3 = 0;
+
+        for (size_t b = a + 1; b < primes.size(); b++)
         {
-          if (primes[b] * primes[c] * primes[d] <= x)
-            p3++;
-          else
-            break;
+          for (size_t c = b; c < primes.size(); c++)
+          {
+            for (size_t d = c; d < primes.size(); d++)
+            {
+              if (primes[b] * primes[c] * primes[d] <= x)
+                p3++;
+              else
+                break;
+            }
+          }
         }
+
+        std::cout << "P3(" << x << ", " << a << ") = " << p3;
+        check(p3 == P3(x, primes[a], a, threads));
       }
     }
+  }
 
-    std::cout << "P3(" << x << ", " << a << ") = " << p3;
-    check(p3 == P3(x, primes[a], a, threads));
+  // Test medium values of x
+  {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(10000, 20000);
+
+    for (int i = 0; i < 10; i++)
+    {
+      int threads = 1;
+      int64_t x = dist(gen);
+      auto primes = generate_primes<int64_t>(x);
+
+      for (int64_t a = 1; primes[a] <= iroot<3>(x); a++)
+      {
+        int64_t p3 = 0;
+
+        for (size_t b = a + 1; b < primes.size(); b++)
+        {
+          for (size_t c = b; c < primes.size(); c++)
+          {
+            for (size_t d = c; d < primes.size(); d++)
+            {
+              if (primes[b] * primes[c] * primes[d] <= x)
+                p3++;
+              else
+                break;
+            }
+          }
+        }
+
+        std::cout << "P3(" << x << ", " << a << ") = " << p3;
+        check(p3 == P3(x, primes[a], a, threads));
+      }
+    }
   }
 
   std::cout << std::endl;
