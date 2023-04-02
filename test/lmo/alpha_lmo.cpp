f229a9c38ae67b20406f3675f467d6be50c2c58c
kimwalisch <kim.walisch@gmail.com>
2023-04-02 17:31:09 +0200

    Speedup test

--- test/lmo/alpha_lmo.cpp
+++ test/lmo/alpha_lmo.cpp
@@ -64,7 +64,7 @@ int main()
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int64_t> dist(min, max);
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 50; i++)
     {
       int64_t x = dist(gen);
       int64_t res1 = pi_meissel(x, threads);
