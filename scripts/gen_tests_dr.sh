07ab55e8707b7b93f2038955dee2ce3f0efb23b7
kimwalisch <kim.walisch@gmail.com>
2023-04-04 16:31:26 +0200

    Add more tests

--- scripts/gen_tests_dr.sh
+++ scripts/gen_tests_dr.sh
@@ -23,6 +23,22 @@ do
 
         echo "{ ${x}LL, ${y}, ${z}, ${c}, ${res}LL },"
 
+        res=$(./primecount 1e$i --$formula --alpha=1)
+        x=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^x =' | cut -f3 -d' ')
+        y=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^y =' | cut -f3 -d' ')
+        z=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^z =' | cut -f3 -d' ')
+        c=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^c =' | cut -f3 -d' ')
+
+        verify=$(./primecount 1e$i --alpha=1 -d -s | grep "= $res\$")
+        if [[ -z "$verify" ]] || [[ "$(./primecount 1e$i -d --alpha=1)" != "$(./primecount 1e$i -m)" ]]
+        then
+            echo ""
+            echo "Error!"
+            exit 1
+        fi
+
+        echo "{ ${x}LL, ${y}, ${z}, ${c}, ${res}LL },"
+
         res=$(./primecount 1e$i --$formula --alpha=10000000)
         x=$(./primecount 1e$i --$formula --alpha=10000000 -s | grep '^x =' | cut -f3 -d' ')
         y=$(./primecount 1e$i --$formula --alpha=10000000 -s | grep '^y =' | cut -f3 -d' ')
--- scripts/gen_tests_s1.sh
+++ scripts/gen_tests_s1.sh
@@ -22,6 +22,21 @@ do
 
         echo "{ ${x}LL, ${y}, ${c}, ${res}LL },"
 
+        res=$(./primecount 1e$i --$formula --alpha=1)
+        x=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^x =' | cut -f3 -d' ')
+        y=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^y =' | cut -f3 -d' ')
+        c=$(./primecount 1e$i --$formula --alpha=1 -s | grep '^c =' | cut -f3 -d' ')
+
+        verify=$(./primecount 1e$i --alpha=1 -d -s | grep "$formula = $res\$")
+        if [[ -z "$verify" ]] || [[ "$(./primecount 1e$i -d --alpha=1)" != "$(./primecount 1e$i -m)" ]]
+        then
+            echo ""
+            echo "Error!"
+            exit 1
+        fi
+
+        echo "{ ${x}LL, ${y}, ${c}, ${res}LL },"
+
         res=$(./primecount 1e$i --$formula --alpha=10000000)
         x=$(./primecount 1e$i --$formula --alpha=10000000 -s | grep '^x =' | cut -f3 -d' ')
         y=$(./primecount 1e$i --$formula --alpha=10000000 -s | grep '^y =' | cut -f3 -d' ')
