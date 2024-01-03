87c4b215294a62fa2bce571a53a769b3a66a02f4
Kim Walisch <kim.walisch@gmail.com>
2024-01-03 15:37:58 +0100

    Update to latest libprimesieve

--- lib/primesieve/cmake/libatomic.cmake
+++ lib/primesieve/cmake/libatomic.cmake
@@ -1,3 +1,10 @@
+# This file first tries to find out whether it is necessary to
+# add libatomic to the linker flags. Usually this is required
+# on old 32-bit CPUs. Then it tries to find libatomic, ideally
+# we would have liked to use an offical CMake module to find
+# libatomic, however there is none.
+# See discussion: https://github.com/kimwalisch/primesieve/issues/141
+
 include(CheckCXXSourceCompiles)
 include(CMakePushCheckState)
 
@@ -7,6 +14,8 @@ if(CMAKE_CXX11_STANDARD_COMPILE_OPTION)
     set(CMAKE_REQUIRED_FLAGS ${CMAKE_CXX11_STANDARD_COMPILE_OPTION})
 endif()
 
+# Check if code compiles without libatomic.
+# Should always work on CPUs >= 64-bits
 check_cxx_source_compiles("
     #include <atomic>
     #include <stdint.h>
@@ -18,27 +27,38 @@ check_cxx_source_compiles("
     }"
     atomic64)
 
+# Our code requires libatomic to compile
 if(NOT atomic64)
-    find_library(ATOMIC NAMES atomic libatomic.so.1)
+    find_library(LIBATOMIC NAMES atomic atomic.so.1 libatomic.so.1)
 
-    if(ATOMIC)
-        set(LIBATOMIC ${ATOMIC})
+    if(LIBATOMIC)
         message(STATUS "Found libatomic: ${LIBATOMIC}")
     else()
-        check_cxx_source_compiles("
-            #include <atomic>
-            #include <stdint.h>
-            int main() {
-                std::atomic<int32_t> x;
-                x = 1;
-                x--;
-                return (int) x;
-            }"
-            atomic32)
+        # Some package managers like homebrew and macports store the compiler's
+        # libraries in a subdirectory of the library directory. E.g. GCC
+        # installed via homebrew stores libatomic at lib/gcc/13/libatomic.dylib
+        # instead of lib/libatomic.dylib. CMake's find_library() cannot easily
+        # be used to recursively find libraries. Therefore we use this workaround
+        # here (try adding -latomic to linker options) for this use case.
+        set(LIBATOMIC "-latomic")
+        message(STATUS "Add linker flag: ${LIBATOMIC}")
+    endif()
 
-        if(atomic32)
-            message(FATAL_ERROR "Failed to find libatomic!")
-        endif()
+    set(CMAKE_REQUIRED_LIBRARIES "${LIBATOMIC}")
+
+    check_cxx_source_compiles("
+        #include <atomic>
+        #include <stdint.h>
+        int main() {
+            std::atomic<int64_t> x;
+            x = 1;
+            x--;
+            return (int) x;
+        }"
+        atomic64_with_libatomic)
+
+    if (NOT atomic64_with_libatomic)
+        message(FATAL_ERROR "Failed to compile std::atomic, libatomic likely not found!")
     endif()
 endif()
 
