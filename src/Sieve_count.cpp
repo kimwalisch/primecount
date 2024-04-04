8c51b7a0073eeda58033d17d883bf7aaa9fb1503
Kim Walisch <kim.walisch@gmail.com>
2024-04-04 10:02:44 +0200

    Refactor

--- src/Sieve_count.cpp
+++ src/Sieve_count.cpp
@@ -71,10 +71,16 @@ namespace primecount {
   for (uint64_t i = start_idx + 1; i < stop_idx; i++) \
     res += popcnt64(sieve64[i]);
 
+// GCC/Clang function multiversioning
 #if defined(MULTIARCH)
-  __attribute__ ((target ("default")))
+  #define ATTRIBUTE_DEFAULT \
+    __attribute__ ((target ("default")))
+#else
+  #define ATTRIBUTE_DEFAULT
 #endif
+
 /// Count 1 bits inside [0, stop]
+ATTRIBUTE_DEFAULT
 uint64_t Sieve::count(uint64_t stop)
 {
   ASSERT(stop >= prev_stop_);
@@ -106,10 +112,8 @@ uint64_t Sieve::count(uint64_t stop)
   return count_;
 }
 
-#if defined(MULTIARCH)
-  __attribute__ ((target ("default")))
-#endif
 /// Count 1 bits inside [start, stop]
+ATTRIBUTE_DEFAULT
 uint64_t Sieve::count(uint64_t start, uint64_t stop) const
 {
   COUNT_1_BITS(start, stop, DEFAULT_POPCNT_KERNEL)
@@ -118,7 +122,79 @@ uint64_t Sieve::count(uint64_t start, uint64_t stop) const
 
 #endif
 
-#if defined(HAS_ARM_SVE)
+#if defined(HAS_AVX512_VPOPCNT) || \
+    defined(MULTIARCH_AVX512_VPOPCNT)
+
+/// Compute the loop below using the AVX512 instruction set.
+/// for (i = start_idx + 1; i < stop_idx; i++)
+///   res += popcnt64(sieve64[i]);
+///
+#define AVX512_POPCNT_KERNEL(sieve64, start_idx, stop_idx)     \
+  uint64_t i = start_idx + 1;                                  \
+  if (i < stop_idx)                                            \
+  {                                                            \
+    __m512i vcnt = _mm512_setzero_si512();                     \
+    for (; i + 8 < stop_idx; i += 8)                           \
+    {                                                          \
+      __m512i vec = _mm512_loadu_epi64(&sieve64[i]);           \
+      vcnt = _mm512_add_epi64(vcnt, _mm512_popcnt_epi64(vec)); \
+    }                                                          \
+    __mmask8 mask = 0xff >> (i + 8 - stop_idx);                \
+    __m512i vec = _mm512_maskz_loadu_epi64(mask, &sieve64[i]); \
+    vcnt = _mm512_add_epi64(vcnt, _mm512_popcnt_epi64(vec));   \
+    res += _mm512_reduce_add_epi64(vcnt);                      \
+  }
+
+// GCC/Clang function multiversioning
+#if defined(MULTIARCH_AVX512_VPOPCNT)
+  #define ATTRIBUTE_AVX512 \
+    __attribute__ ((target ("avx512f,avx512vpopcntdq")))
+#else
+  #define ATTRIBUTE_AVX512
+#endif
+
+/// Count 1 bits inside [0, stop]
+ATTRIBUTE_AVX512
+uint64_t Sieve::count(uint64_t stop)
+{
+  ASSERT(stop >= prev_stop_);
+  uint64_t start = prev_stop_ + 1;
+  prev_stop_ = stop;
+
+  // Quickly count the number of unsieved elements (in
+  // the sieve array) up to a value that is close to
+  // the stop number i.e. (stop - start) < counter_.dist.
+  // We do this using the counter array, each element
+  // of the counter array contains the number of
+  // unsieved elements in the interval:
+  // [i * counter_.dist, (i + 1) * counter_.dist[.
+  while (counter_.stop <= stop)
+  {
+    start = counter_.stop;
+    counter_.stop += counter_.dist;
+    counter_.sum += counter_[counter_.i++];
+    count_ = counter_.sum;
+  }
+
+  // Here the remaining distance is relatively small i.e.
+  // (stop - start) < counter_.dist, hence we simply
+  // count the remaining number of unsieved elements by
+  // linearly iterating over the sieve array.
+  COUNT_1_BITS(start, stop, AVX512_POPCNT_KERNEL)
+  count_ += res;
+
+  return count_;
+}
+
+/// Count 1 bits inside [start, stop]
+ATTRIBUTE_AVX512
+uint64_t Sieve::count(uint64_t start, uint64_t stop) const
+{
+  COUNT_1_BITS(start, stop, AVX512_POPCNT_KERNEL)
+  return res;
+}
+
+#elif defined(HAS_ARM_SVE)
 
 /// Compute the loop below using the ARM SVE instruction set.
 /// for (i = start_idx + 1; i < stop_idx; i++)
@@ -176,74 +252,6 @@ uint64_t Sieve::count(uint64_t stop)
 uint64_t Sieve::count(uint64_t start, uint64_t stop) const
 {
   COUNT_1_BITS(start, stop, SVE_POPCNT_KERNEL)
-  return res;
-}
-
-#elif defined(HAS_AVX512_VPOPCNT) || \
-      defined(MULTIARCH_AVX512_VPOPCNT)
-
-/// Compute the loop below using the AVX512 instruction set.
-/// for (i = start_idx + 1; i < stop_idx; i++)
-///   res += popcnt64(sieve64[i]);
-///
-#define AVX512_POPCNT_KERNEL(sieve64, start_idx, stop_idx)     \
-  uint64_t i = start_idx + 1;                                  \
-  if (i < stop_idx)                                            \
-  {                                                            \
-    __m512i vcnt = _mm512_setzero_si512();                     \
-    for (; i + 8 < stop_idx; i += 8)                           \
-    {                                                          \
-      __m512i vec = _mm512_loadu_epi64(&sieve64[i]);           \
-      vcnt = _mm512_add_epi64(vcnt, _mm512_popcnt_epi64(vec)); \
-    }                                                          \
-    __mmask8 mask = 0xff >> (i + 8 - stop_idx);                \
-    __m512i vec = _mm512_maskz_loadu_epi64(mask, &sieve64[i]); \
-    vcnt = _mm512_add_epi64(vcnt, _mm512_popcnt_epi64(vec));   \
-    res += _mm512_reduce_add_epi64(vcnt);                      \
-  }
-
-#if defined(MULTIARCH_AVX512_VPOPCNT)
-  __attribute__ ((target ("avx512f,avx512vpopcntdq")))
-#endif
-/// Count 1 bits inside [0, stop]
-uint64_t Sieve::count(uint64_t stop)
-{
-  ASSERT(stop >= prev_stop_);
-  uint64_t start = prev_stop_ + 1;
-  prev_stop_ = stop;
-
-  // Quickly count the number of unsieved elements (in
-  // the sieve array) up to a value that is close to
-  // the stop number i.e. (stop - start) < counter_.dist.
-  // We do this using the counter array, each element
-  // of the counter array contains the number of
-  // unsieved elements in the interval:
-  // [i * counter_.dist, (i + 1) * counter_.dist[.
-  while (counter_.stop <= stop)
-  {
-    start = counter_.stop;
-    counter_.stop += counter_.dist;
-    counter_.sum += counter_[counter_.i++];
-    count_ = counter_.sum;
-  }
-
-  // Here the remaining distance is relatively small i.e.
-  // (stop - start) < counter_.dist, hence we simply
-  // count the remaining number of unsieved elements by
-  // linearly iterating over the sieve array.
-  COUNT_1_BITS(start, stop, AVX512_POPCNT_KERNEL)
-  count_ += res;
-
-  return count_;
-}
-
-#if defined(MULTIARCH_AVX512_VPOPCNT)
-  __attribute__ ((target ("avx512f,avx512vpopcntdq")))
-#endif
-/// Count 1 bits inside [start, stop]
-uint64_t Sieve::count(uint64_t start, uint64_t stop) const
-{
-  COUNT_1_BITS(start, stop, AVX512_POPCNT_KERNEL)
   return res;
 }
 
