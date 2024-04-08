b596db6e116b4f9706b3b007807c2b48ee87c552
Kim Walisch <kim.walisch@gmail.com>
2024-04-08 19:31:51 +0200

    Simplify int128_t detection

--- cmake/int128_t.cmake
+++ cmake/int128_t.cmake
@@ -1,17 +1,9 @@
-# GCC & Clang support 128-bit integers on 64-bit CPUs. However
-# support for 128-bit integers in the C++ standard library is
-# currently (2021) only enabled if the GNU extensions are enabled.
-# The GNU extensions are usually enabled by default but will be
-# disabled if the user compiles with the -std=c++* option. You
-# should use -std=gnu++* instead of -std=c++* if you want to enable
-# 128-bit integer support in primecount (or simply omit both
-# -std=c++* and -std=gnu++*).
-
 include(CheckCXXSourceCompiles)
 include(CMakePushCheckState)
 
 cmake_push_check_state()
 
+# Check if we need to enable C++11 for our tests.
 check_cxx_source_compiles("
     #include <limits>
     #include <type_traits>
@@ -21,7 +13,6 @@ check_cxx_source_compiles("
         return 0;
     }" compiler_supports_cpp11)
 
-# Enable C++11 if static_assert() fails to compile
 if(NOT compiler_supports_cpp11)
     if(CMAKE_CXX11_EXTENSION_COMPILE_OPTION)
         set(CMAKE_REQUIRED_FLAGS "${CMAKE_CXX11_EXTENSION_COMPILE_OPTION} ${CMAKE_CXX_FLAGS}")
@@ -32,12 +23,26 @@ endif()
 
 set(CMAKE_REQUIRED_INCLUDES "${PROJECT_SOURCE_DIR}/include")
 
+# Test if the C++ compiler supports 128-bit integers.
+# This test does not use the C++ standard library.
 check_cxx_source_compiles("
     #include <int128_t.hpp>
-    #include <limits>
-    #include <type_traits>
-
     int main() {
+        using primecount::int128_t;
+        int128_t x = int128_t(1) << 100;
+        x /= 123;
+        return (x > 0) ? 0 : 1;
+    }" int128)
+
+if(int128)
+    # Test if the C++ standard library supports
+    # 128-bit integers out of the box.
+    check_cxx_source_compiles("
+        #include <int128_t.hpp>
+        #include <algorithm>
+        #include <limits>
+        #include <type_traits>
+
         using namespace primecount;
         static_assert(std::numeric_limits<int128_t>::max() == int128_t((uint128_t(1) << 127) - 1), \"\");
         static_assert(std::numeric_limits<uint128_t>::max() == ~uint128_t(0), \"\");
@@ -48,18 +53,28 @@ check_cxx_source_compiles("
         static_assert(std::is_signed<int128_t>::value, \"\");
         static_assert(std::is_unsigned<uint128_t>::value, \"\");
         static_assert(std::is_unsigned<typename std::make_unsigned<int128_t>::type>::value, \"\");
-        return 0;
-    }" int128)
-
-if(NOT int128)
-    # Now let's try again if it works with our <int128_STL_patch.hpp> header
-    check_cxx_source_compiles("
-        #include <int128_t.hpp>
-        #include <int128_STL_patch.hpp>
-        #include <limits>
-        #include <type_traits>
 
         int main() {
+            int128_t x = 123;
+            int128_t y = 1000;
+            int128_t z = std::max(x, y);
+            if (z == y)
+                return 0;
+            else
+                return 1;
+        }" int128_STL)
+
+    if(NOT int128_STL)
+        # Now let's try again if it works with our <int128_STL_patch.hpp>
+        # header. This is required if the user compiles with
+        # -std=c++11 instead of -std=gnu++11.
+        check_cxx_source_compiles("
+            #include <int128_t.hpp>
+            #include <int128_STL_patch.hpp>
+            #include <algorithm>
+            #include <limits>
+            #include <type_traits>
+
             using namespace primecount;
             static_assert(std::numeric_limits<int128_t>::max() == int128_t((uint128_t(1) << 127) - 1), \"\");
             static_assert(std::numeric_limits<uint128_t>::max() == ~uint128_t(0), \"\");
@@ -70,49 +85,27 @@ if(NOT int128)
             static_assert(std::is_signed<int128_t>::value, \"\");
             static_assert(std::is_unsigned<uint128_t>::value, \"\");
             static_assert(std::is_unsigned<typename std::make_unsigned<int128_t>::type>::value, \"\");
-            return 0;
-        }" int128_STL_patch)
 
-    if(int128_STL_patch)
-        set(ENABLE_INT128_STL_PATCH "ENABLE_INT128_STL_PATCH")
-    else()
-        set(DISABLE_INT128 "DISABLE_INT128")
-
-        # Print a warning message if the user has specified the -std=c++*
-        # option and 128-bit integers are disabled because this of this
-        # option. We try to overwrite -std=c++* by -std=gnu++11 and check
-        # if this would enable 128-bit integers.
-        if(CMAKE_CXX11_EXTENSION_COMPILE_OPTION AND "${CMAKE_CXX_FLAGS}" MATCHES "-std=c\\+\\+[0-9]+")
-            set(CMAKE_REQUIRED_FLAGS "${CMAKE_CXX11_EXTENSION_COMPILE_OPTION}")
-            set(CMAKE_REQUIRED_QUIET TRUE)
-
-            check_cxx_source_compiles("
-                #include <int128_t.hpp>
-                #include <limits>
-                #include <type_traits>
-
-                int main() {
-                    using namespace primecount;
-                    static_assert(std::numeric_limits<int128_t>::max() == int128_t((uint128_t(1) << 127) - 1), \"\");
-                    static_assert(std::numeric_limits<uint128_t>::max() == ~uint128_t(0), \"\");
-                    static_assert(std::numeric_limits<int128_t>::digits == 127, \"\");
-                    static_assert(std::numeric_limits<uint128_t>::digits == 128, \"\");
-                    static_assert(std::is_integral<int128_t>::value, \"\");
-                    static_assert(std::is_integral<uint128_t>::value, \"\");
-                    static_assert(std::is_signed<int128_t>::value, \"\");
-                    static_assert(std::is_unsigned<uint128_t>::value, \"\");
-                    static_assert(std::is_unsigned<typename std::make_unsigned<int128_t>::type>::value, \"\");
+            int main() {
+                int128_t x = 123;
+                int128_t y = 1000;
+                int128_t z = std::max(x, y);
+                if (z == y)
                     return 0;
-                }" int128_with_gnu11)
+                else
+                    return 1;
+            }" int128_STL_patch)
 
-            if(int128_with_gnu11)
-                message(WARNING "Your usage of -std=c++* disables 128-bit integer support in "
-                                "primecount, primecount will only support numbers <= 2^63. "
-                                "Use -std=gnu++* instead to enable 128-bit integer support "
-                                "(or omit both -std=c++* and -std=gnu++*).")
-            endif()
+        if(int128_STL_patch)
+            set(ENABLE_INT128_STL_PATCH "ENABLE_INT128_STL_PATCH")
         endif()
     endif()
+endif()
+
+if(int128 AND NOT int128_STL AND NOT int128_STL_patch)
+    set(DISABLE_INT128 "DISABLE_INT128")
+    message(WARNING "Your C++ standard library does not support 128-bit integers, "
+                    "hence primecount will only support numbers <= 2^63.")
 endif()
 
 cmake_pop_check_state()
