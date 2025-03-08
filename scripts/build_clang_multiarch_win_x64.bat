47047cb79b003ea3dbd9ed522426d9ab2f895cb6
Kim Walisch <kim.walisch@gmail.com>
2025-03-08 12:28:49 +0100

    Update build scripts

--- scripts/build_clang_multiarch_win_x64.bat
+++ scripts/build_clang_multiarch_win_x64.bat
@@ -9,7 +9,10 @@
 :: C:\Program Files\LLVM\lib\clang\18\include\immintrin.h and unconditionally
 :: include all MMX, SSE, POPCNT, BMI, BMI2, AVX, AVX and AVX512 headers.
 
-del /Q ..\src\deleglise-rivat\S2_easy.cpp ..\src\gourdon\AC.cpp
+del /Q ..\src\deleglise-rivat\S2_easy.cpp
+del /Q ..\src\deleglise-rivat\S2_hard_multiarch_arm_sve.cpp
+del /Q ..\src\gourdon\AC.cpp
+del /Q ..\src\gourdon\D_multiarch_arm_sve.cpp
 clang++ -I../include -I../lib/primesieve/include -O3 -mpopcnt -fopenmp -Wall -Wextra -pedantic -DNDEBUG -DENABLE_INT128_OPENMP_PATCH -DENABLE_MULTIARCH_AVX512_BW -DENABLE_MULTIARCH_AVX512_VBMI2 -DENABLE_MULTIARCH_AVX512_VPOPCNT ../lib/primesieve/src/*.cpp ../lib/primesieve/src/arch/x86/*.cpp ../src/*.cpp ../src/arch/x86/*.cpp ../src/lmo/*.cpp ../src/deleglise-rivat/*.cpp ../src/gourdon/*.cpp ../src/app/*.cpp -o primecount.exe "C:\Program Files\LLVM\lib\clang\18\lib\windows\clang_rt.builtins-x86_64.lib"
 git checkout ..\src\deleglise-rivat
 git checkout ..\src\gourdon
--- scripts/build_mingw64_arm64.sh
+++ scripts/build_mingw64_arm64.sh
@@ -49,7 +49,11 @@ mkdir build-release-arm64
 cd build-release-arm64
 
 rm ../src/deleglise-rivat/S2_easy.cpp
+rm ../src/deleglise-rivat/S2_hard_multiarch_arm_sve.cpp
+rm ../src/deleglise-rivat/S2_hard_multiarch_avx512.cpp
 rm ../src/gourdon/AC.cpp
+rm ../src/gourdon/D_multiarch_arm_sve.cpp
+rm ../src/gourdon/D_multiarch_avx512.cpp
 clang++ -flto -static -O3 -DNDEBUG -D_WIN32_WINNT=0x0A00 -Wall -Wextra -pedantic -fopenmp -I ../include -I ../lib/primesieve/include ../lib/primesieve/src/*.cpp ../src/*.cpp ../src/lmo/*.cpp ../src/deleglise-rivat/*.cpp ../src/gourdon/*.cpp ../src/app/*.cpp -o primecount.exe -lPsapi
 git checkout ..
 
