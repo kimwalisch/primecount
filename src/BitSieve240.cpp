299a052871a4b7a8ad21af8a299c4b842e843d08
kimwalisch <kim.walisch@gmail.com>
2025-05-25 19:43:53 +0200

    New 128-bit test sieve

--- src/BitSieve240.cpp
+++ src/BitSieve240.cpp
@@ -24,6 +24,21 @@ namespace {
 /// The 8 bits in each byte of the sieve array correspond
 /// to the offsets { 1, 7, 11, 13, 17, 19, 23, 29 }.
 ///
+constexpr int left_shift(int n)
+{
+  return   (n % 30 <= 1)  ? (n / 30 * 8) + 0
+         : (n % 30 <= 7)  ? (n / 30 * 8) + 1
+         : (n % 30 <= 11) ? (n / 30 * 8) + 2
+         : (n % 30 <= 13) ? (n / 30 * 8) + 3
+         : (n % 30 <= 17) ? (n / 30 * 8) + 4
+         : (n % 30 <= 19) ? (n / 30 * 8) + 5
+         : (n % 30 <= 23) ? (n / 30 * 8) + 6
+         : (n / 30 * 8) + 7;
+}
+
+/// The 8 bits in each byte of the sieve array correspond
+/// to the offsets { 1, 7, 11, 13, 17, 19, 23, 29 }.
+///
 constexpr int right_shift(int n)
 {
   return   (n % 30 >= 29) ? 56 - (n / 30 * 8)
@@ -35,6 +50,12 @@ constexpr int right_shift(int n)
          : (n % 30 >= 7)  ? 62 - (n / 30 * 8)
          : (n % 30 >= 1)  ? 63 - (n / 30 * 8)
          : 64 - (n / 30 * 8);
+}
+
+/// Bitmask to unset bits <= n
+constexpr uint64_t unset_s(int n)
+{
+  return ~0ull << left_shift(n);
 }
 
 /// Bitmask to unset bits >= n
@@ -49,6 +70,18 @@ namespace primecount {
 
 /// pi(x) for x < 6
 const Array<uint64_t, 6> BitSieve240::pi_tiny_ = { 0, 0, 1, 2, 2, 3 };
+
+const Array<uint64_t, 64> BitSieve240::bit_values_ =
+{
+  0 * 30 + 1, 0 * 30 + 7, 0 * 30 + 11, 0 * 30 + 13, 0 * 30 + 17, 0 * 30 + 19, 0 * 30 + 23, 0 * 30 + 29,
+  1 * 30 + 1, 1 * 30 + 7, 1 * 30 + 11, 1 * 30 + 13, 1 * 30 + 17, 1 * 30 + 19, 1 * 30 + 23, 1 * 30 + 29,
+  2 * 30 + 1, 2 * 30 + 7, 2 * 30 + 11, 2 * 30 + 13, 2 * 30 + 17, 2 * 30 + 19, 2 * 30 + 23, 2 * 30 + 29,
+  3 * 30 + 1, 3 * 30 + 7, 3 * 30 + 11, 3 * 30 + 13, 3 * 30 + 17, 3 * 30 + 19, 3 * 30 + 23, 3 * 30 + 29,
+  4 * 30 + 1, 4 * 30 + 7, 4 * 30 + 11, 4 * 30 + 13, 4 * 30 + 17, 4 * 30 + 19, 4 * 30 + 23, 4 * 30 + 29,
+  5 * 30 + 1, 5 * 30 + 7, 5 * 30 + 11, 5 * 30 + 13, 5 * 30 + 17, 5 * 30 + 19, 5 * 30 + 23, 5 * 30 + 29,
+  6 * 30 + 1, 6 * 30 + 7, 6 * 30 + 11, 6 * 30 + 13, 6 * 30 + 17, 6 * 30 + 19, 6 * 30 + 23, 6 * 30 + 29,
+  7 * 30 + 1, 7 * 30 + 7, 7 * 30 + 11, 7 * 30 + 13, 7 * 30 + 17, 7 * 30 + 19, 7 * 30 + 23, 7 * 30 + 29
+};
 
 /// Bitmasks needed to set a specific bit in the sieve array
 const Array<uint64_t, 240> BitSieve240::set_bit_ =
@@ -154,6 +187,59 @@ const Array<uint64_t, 240> BitSieve240::unset_bit_ =
   ~0ull, ~0ull, ~(1ull << 60), ~0ull, ~(1ull << 61),
   ~0ull, ~0ull, ~0ull, ~(1ull << 62), ~0ull,
   ~0ull, ~0ull, ~0ull, ~0ull, ~(1ull << 63)
+};
+
+/// Unset bits < start
+const Array<uint64_t, 240> BitSieve240::unset_smaller_ =
+{
+  unset_s(0), unset_s(1), unset_s(2), unset_s(3), unset_s(4),
+  unset_s(5), unset_s(6), unset_s(7), unset_s(8), unset_s(9),
+  unset_s(10), unset_s(11), unset_s(12), unset_s(13), unset_s(14),
+  unset_s(15), unset_s(16), unset_s(17), unset_s(18), unset_s(19),
+  unset_s(20), unset_s(21), unset_s(22), unset_s(23), unset_s(24),
+  unset_s(25), unset_s(26), unset_s(27), unset_s(28), unset_s(29),
+  unset_s(30), unset_s(31), unset_s(32), unset_s(33), unset_s(34),
+  unset_s(35), unset_s(36), unset_s(37), unset_s(38), unset_s(39),
+  unset_s(40), unset_s(41), unset_s(42), unset_s(43), unset_s(44),
+  unset_s(45), unset_s(46), unset_s(47), unset_s(48), unset_s(49),
+  unset_s(50), unset_s(51), unset_s(52), unset_s(53), unset_s(54),
+  unset_s(55), unset_s(56), unset_s(57), unset_s(58), unset_s(59),
+  unset_s(60), unset_s(61), unset_s(62), unset_s(63), unset_s(64),
+  unset_s(65), unset_s(66), unset_s(67), unset_s(68), unset_s(69),
+  unset_s(70), unset_s(71), unset_s(72), unset_s(73), unset_s(74),
+  unset_s(75), unset_s(76), unset_s(77), unset_s(78), unset_s(79),
+  unset_s(80), unset_s(81), unset_s(82), unset_s(83), unset_s(84),
+  unset_s(85), unset_s(86), unset_s(87), unset_s(88), unset_s(89),
+  unset_s(90), unset_s(91), unset_s(92), unset_s(93), unset_s(94),
+  unset_s(95), unset_s(96), unset_s(97), unset_s(98), unset_s(99),
+  unset_s(100), unset_s(101), unset_s(102), unset_s(103), unset_s(104),
+  unset_s(105), unset_s(106), unset_s(107), unset_s(108), unset_s(109),
+  unset_s(110), unset_s(111), unset_s(112), unset_s(113), unset_s(114),
+  unset_s(115), unset_s(116), unset_s(117), unset_s(118), unset_s(119),
+  unset_s(120), unset_s(121), unset_s(122), unset_s(123), unset_s(124),
+  unset_s(125), unset_s(126), unset_s(127), unset_s(128), unset_s(129),
+  unset_s(130), unset_s(131), unset_s(132), unset_s(133), unset_s(134),
+  unset_s(135), unset_s(136), unset_s(137), unset_s(138), unset_s(139),
+  unset_s(140), unset_s(141), unset_s(142), unset_s(143), unset_s(144),
+  unset_s(145), unset_s(146), unset_s(147), unset_s(148), unset_s(149),
+  unset_s(150), unset_s(151), unset_s(152), unset_s(153), unset_s(154),
+  unset_s(155), unset_s(156), unset_s(157), unset_s(158), unset_s(159),
+  unset_s(160), unset_s(161), unset_s(162), unset_s(163), unset_s(164),
+  unset_s(165), unset_s(166), unset_s(167), unset_s(168), unset_s(169),
+  unset_s(170), unset_s(171), unset_s(172), unset_s(173), unset_s(174),
+  unset_s(175), unset_s(176), unset_s(177), unset_s(178), unset_s(179),
+  unset_s(180), unset_s(181), unset_s(182), unset_s(183), unset_s(184),
+  unset_s(185), unset_s(186), unset_s(187), unset_s(188), unset_s(189),
+  unset_s(190), unset_s(191), unset_s(192), unset_s(193), unset_s(194),
+  unset_s(195), unset_s(196), unset_s(197), unset_s(198), unset_s(199),
+  unset_s(200), unset_s(201), unset_s(202), unset_s(203), unset_s(204),
+  unset_s(205), unset_s(206), unset_s(207), unset_s(208), unset_s(209),
+  unset_s(210), unset_s(211), unset_s(212), unset_s(213), unset_s(214),
+  unset_s(215), unset_s(216), unset_s(217), unset_s(218), unset_s(219),
+  unset_s(220), unset_s(221), unset_s(222), unset_s(223), unset_s(224),
+  unset_s(225), unset_s(226), unset_s(227), unset_s(228), unset_s(229),
+  unset_s(230), unset_s(231), unset_s(232), unset_s(233), unset_s(234),
+  unset_s(235), unset_s(236), unset_s(237), unset_s(238), unset_s(239)
 };
 
 /// Unset bits > stop
---
+++ test/sieve128.cpp
@@ -0,0 +1,71 @@
+#include <int128_t.hpp>
+#include <Vector.hpp>
+#include <BitSieve240.hpp>
+#include <calculator.hpp>
+#include <primesieve.hpp>
+#include <imath.hpp>
+#include <iostream>
+
+using namespace primecount;
+
+class Sieve128bit : public BitSieve240 {
+public:
+  static void sieve128(uint128_t start, uint128_t stop)
+  {
+    uint128_t old_start = start;
+    if (start % 240)
+      start -= start % 240;
+
+    uint128_t dist = (stop - start) + 1;
+    std::size_t size = (std::size_t) ceil_div(dist, 240);
+    uint64_t sqrt_stop = (uint64_t) isqrt(stop);
+    Vector<uint64_t> sieve(size);
+
+    std::fill(sieve.begin(), sieve.end(), ~0ull);
+    sieve.front() &= unset_smaller_[old_start % 240];
+    sieve.back() &= unset_larger_[stop % 240];
+    primesieve::iterator iter(7, sqrt_stop);
+    uint64_t prime;
+
+    while ((prime = iter.next_prime()) <= sqrt_stop)
+    {
+      uint128_t q = (start / prime) + 1;
+      uint128_t n = prime * q;
+      n += prime * (n % 2 == 0);
+      ASSERT(n % 2 == 0);
+
+      uint64_t i = (uint64_t) (n - start);
+      uint64_t limit = (uint64_t) (stop - start);
+
+      for (; i <= limit; i += prime * 2)
+        sieve[i / 240] &= unset_bit_[i % 240];
+    }
+
+    for (std::size_t i = 0; i < sieve.size(); i++)
+    {
+      uint64_t bits = sieve[i];
+      for (; bits; bits &= bits - 1)
+      {
+        auto bit_index = __builtin_ctzll(bits);
+        uint64_t bit_value = bit_values_[bit_index];
+        uint128_t prime = start + i * 240 + bit_value;
+        std::cout << prime << std::endl;
+      }
+    }
+  }
+};
+
+int main(int argc, char** argv)
+{
+  if (argc < 3)
+  {
+    std::cerr << "Missing start/stop params!" << std::endl;
+    return 1;
+  }
+
+  uint128_t start = calculator::eval<uint128_t>(argv[1]);
+  uint128_t stop = calculator::eval<uint128_t>(argv[2]);
+  Sieve128bit::sieve128(start, stop);
+
+  return 0;
+}
