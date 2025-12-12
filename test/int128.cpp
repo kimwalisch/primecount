9e594d0fd150c09503d524494ea4caeab084a406
Kim Walisch <kim.walisch@gmail.com>
2025-12-12 20:35:27 +0100

    Get rid of <sstream>

--- test/int128.cpp
+++ test/int128.cpp
@@ -19,7 +19,6 @@ using namespace primecount;
 #if defined(HAVE_INT128_T)
 
 #include <cstdlib>
-#include <sstream>
 #include <string>
 
 void check(bool OK)
@@ -57,31 +56,31 @@ int main()
                 "is_unsigned<uint128_t> != true");
 
   {
-    std::ostringstream s;
-    s << (((int128_t) 1 ) << 100);
-    std::cout << "2^100 = " << s.str();
-    check(s.str() == "1267650600228229401496703205376");
+    int128_t x = int128_t(1) << 100;
+    std::string str = to_string(x);
+    std::cout << "2^100 = " << str;
+    check(str == "1267650600228229401496703205376");
   }
 
   {
-    std::ostringstream s;
-    s << pstd::numeric_limits<int128_t>::min() + 1;
-    std::cout << "-2^127+1 = " << s.str();
-    check(s.str() == "-170141183460469231731687303715884105727");
+    int128_t x = pstd::numeric_limits<int128_t>::min() + 1;
+    std::string str = to_string(x);
+    std::cout << "-2^127+1 = " << str;
+    check(str == "-170141183460469231731687303715884105727");
   }
 
   {
-    std::ostringstream s;
-    s << pstd::numeric_limits<int128_t>::max();
-    std::cout << "2^127-1 = " << s.str();
-    check(s.str() == "170141183460469231731687303715884105727");
+    int128_t x = pstd::numeric_limits<int128_t>::max();
+    std::string str = to_string(x);
+    std::cout << "2^127-1 = " << str;
+    check(str == "170141183460469231731687303715884105727");
   }
 
   {
-    std::ostringstream s;
-    s << pstd::numeric_limits<uint128_t>::max();
-    std::cout << "2^128-1 = " << s.str();
-    check(s.str() == "340282366920938463463374607431768211455");
+    uint128_t x = pstd::numeric_limits<uint128_t>::max();
+    std::string str = to_string(x);
+    std::cout << "2^128-1 = " << str;
+    check(str == "340282366920938463463374607431768211455");
   }
 
 #endif
