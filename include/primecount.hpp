e1fa49da6e4f8130932c2b432d3325a9018ed2be
kimwalisch <kim.walisch@gmail.com>
2026-07-23 20:53:28 +0200

    Update version to 8.7

--- include/primecount.hpp
+++ include/primecount.hpp
@@ -17,9 +17,9 @@
 #include <string>
 #include <stdint.h>
 
-#define PRIMECOUNT_VERSION "8.6"
+#define PRIMECOUNT_VERSION "8.7"
 #define PRIMECOUNT_VERSION_MAJOR 8
-#define PRIMECOUNT_VERSION_MINOR 6
+#define PRIMECOUNT_VERSION_MINOR 7
 
 namespace primecount {
 
