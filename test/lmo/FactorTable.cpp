1959ec1295cd708a0aceb031a4ece364415fe359
kimwalisch <kim.walisch@gmail.com>
2026-08-05 22:04:18 +0200

    Refactor

--- test/lmo/FactorTable.cpp
+++ test/lmo/FactorTable.cpp
@@ -3,7 +3,7 @@
 /// @brief  FactorTable is a compressed lookup table of mu
 ///         (moebius) and lpf (least prime factor).
 ///
-/// Copyright (C) 2024 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2026 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
