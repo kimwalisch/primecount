43774d90dd41faeeed6822af858e115e4a7b276e
kimwalisch <kim.walisch@gmail.com>
2026-08-08 12:26:01 +0200

    Revert

--- cmake/compiler_supports_cpp11.cmake
+++ cmake/compiler_supports_cpp11.cmake
@@ -1,11 +1,11 @@
 include(CheckCXXSourceCompiles)
 
+# Check if compiler supports C++11 or later
 check_cxx_source_compiles("
     #include <limits>
     #include <type_traits>
     int main() {
         static_assert(std::numeric_limits<unsigned int>::min() == 0, \"\");
         static_assert(std::is_integral<int>::value, \"\");
-        auto lambda = [](auto x) { return x + 1; };
-        return (int) lambda(0) - 1;
-    }" compiler_supports_cpp14)
+        return 0;
+    }" compiler_supports_cpp11)
