ba40656e6a5a62354b7b9cfc720c206de242fe13
Kim Walisch <kim.walisch@gmail.com>
2026-09-08 09:37:34 +0200

    Refactor

--- src/gourdon/AC_libdivide.hpp
+++ src/gourdon/AC_libdivide.hpp
@@ -34,10 +34,10 @@ struct LibdividePrimes
 
     int64_t min_thread_size = (int64_t) 1e6;
     int64_t primes_size = primes.size();
-    int init_threads = ideal_num_threads(primes_size, threads, min_thread_size);
-    int64_t thread_dist = ceil_div(primes_size, init_threads);
+    threads = ideal_num_threads(primes_size, threads, min_thread_size);
+    int64_t thread_dist = ceil_div(primes_size, threads);
 
-    #pragma omp parallel for num_threads(init_threads) schedule(static, 1)
+    #pragma omp parallel for num_threads(threads) schedule(static, 1)
     for (int64_t low = 1; low < primes_size; low += thread_dist)
     {
       int64_t high = min(low + thread_dist, primes_size);
